846e7e4132eb94425cccb18b27a828dcda001b9d
akaysh <ksh.shrm1@gmail.com>
2017-05-13 21:51:41 +0530

    Tried different loss functions and optimizations

--- src/proto/DE-NN-Knet.jl
+++ src/proto/DE-NN-Knet.jl
@@ -1,12 +1,13 @@
-using Knet
+#using Knet,ArgParse,Main
 
 for p in ("Knet","ArgParse")
     Pkg.installed(p) == nothing && Pkg.add(p)
 end
 
 using Knet,ArgParse,Main
-
-
+#some global functions
+z(P,i,x) = P[1][i]*x + P[2][i]
+sig_der(x) = sigm(x)*(1-sigm(x))
 
 #Analytical Solution of the Eq
 diff_sol(x) = exp(-(x^2)/2)/(1+x+(x)^3) + x^2
@@ -17,71 +18,168 @@ phi(P,x) = 1 + x*(predict(P,x))
 #The Differential Equation function for Example 1
 f(P,x) = x^3 + 2*x + (x^2)*((1+3*(x^2))/(1+x+(x^3))) - (phi(P,x))*(x + ((1+3*(x^2))/(1+x+x^3)))
 
- x_tr = rand(10)
- x_tst = rand(10)
+ x_tr = linspace(0.0,1,100)
+ x_tst = linspace(0.1,1,100)
 
 
-function train(P, prms, data; epochs=10, iters=6000)
-    for epoch=1:epochs
+function train(P, prms, data; epochs =10, iters=1000)
+    #print(size(P),size(g),size(prms))
+      for epoch=1:epochs
+      	      #i = 0
         for (x,y) in data
-            g = lossgradient(P, x, y)
-            update!(P, g, prms)
-            if (iters -= 1) <= 0
-                return P
-            end
+
+          #i = i+1
+	        g = lossgradient(P, x, y)
+          #print(size(P),size(g),size(prms))
+          update!(P, g, prms)
+          #println(P[1][1],P[2][1],P[3][1],P[4][1])
+          if (iters -= 1) <= 0
+            return P
+          end
         end
-    end
+      end
+
     return P
 end
 
 function predict(P,x)
   w1, b1, w2, b2 = P
-  h = sigmoid(w1 * x .+ b1)
+  h = sigm(w1 * x .+ b1)
   return w2 * h .+ b2
 end
 
 
 function loss(P,x,ygold)
-    z(i) = P[1][i]*x + P[2][i]
-    sig_der(x) = sigmoid(x)(1-sigmoid(x))
-    dNx = P[3][i]*P[1][i]*(sig_der(z(i)))
-    to_minimize = predict(P,x) + (x*dNx) - f(P,x)
+    dNx = sum([P[3][i]*P[1][i]*sig_der(P[1][i]*x + P[2][i]) for i = 1:10])
+    to_minimize = (predict(P,x) + (x*dNx) - f(P,x))[1]
 end
 
 lossgradient = grad(loss)
 
 function init_params(;ftype=Float32,atype=KnetArray)
-    P = Vector{Vector{Float32}}(4)
+    #P = Vector{Vector{Float32}}(4)
+    P = Array{Any}(4)
     P[1] = randn(Float32,10,1)
     P[2] = zeros(Float32,10,1)
     P[3] = randn(Float32,1,10)
     P[4] = zeros(Float32,1,1)
+    P = map(x -> convert(atype, x), P)
+
     return P
 end
 
-function main(args)
-
-    P = init_params()
-    prm = Adam(lr=0.1, beta1=0.9, beta2=0.95, eps=1e-6)
-    prms = Any[]
-    push!(prms, prm)
-    dtrn = Any[]
-    dtst = Any[]
-    for i=1:5
-        push!(dtrn, (x_trn[i],map(diff_sol,x_trn)[i]))
-        push!(dtrn, (x_tst[i],map(diff_sol,x_tst)[i]))
+function accuracy(w, dtst, pred=predict)
+    ncorrect = ninstance = nloss = 0
+    for (x, ygold) in dtst
+        ypred = pred(w, x)
+        #println(x,ygold,ypred)
+        ynorm = ypred .- log(sum(exp(ypred),1))
+        nloss += -sum(ygold .* ynorm)
+        ncorrect += sum(ygold .* (ypred .== maximum(ypred,1)))
+        ninstance += size(ygold,2)
+        #println("Correct:",ncorrect,"Loss:",nloss,"Instance:",ninstance)
     end
+    println("a1:",ncorrect/ninstance,"a2",nloss/ninstance)
+    return (ncorrect/ninstance, nloss/ninstance)
+end
 
-   
-    report(epoch)=println((:epoch,epoch,:trn,accuracy(P,dtrn,predict),:tst,accuracy(P,dtst,predict)))
+function params(ws, o)
+	prms = Any[]
+
+	for i=1:length(ws)
+		w = ws[i]
+		if o[:optim] == "Sgd"
+			prm = Sgd(;lr=o[:lr])
+		elseif o[:optim] == "Momentum"
+			prm = Momentum(lr=o[:lr], gamma=o[:gamma])
+		elseif o[:optim] == "Adam"
+			prm = Adam(lr=o[:lr], beta1=o[:beta1], beta2=o[:beta2], eps=o[:eps])
+		elseif o[:optim] == "Adagrad"
+			prm = Adagrad(lr=o[:lr], eps=o[:eps])
+		elseif o[:optim] == "Adadelta"
+			prm = Adadelta(lr=o[:lr], rho=o[:rho], eps=o[:eps])
+		elseif o[:optim] == "Rmsprop"
+			prm = Rmsprop(lr=o[:lr], rho=o[:rho], eps=o[:eps])
+		else
+			error("Unknown optimization method!")
+		end
+		push!(prms, prm)
+	end
+
+	return prms
+end
+
+function batch(x, y, batchsize; atype=Array{Float32})
+    data = Any[]
+    x_ = convert(atype,x)
+    y_ = convert(atype,y)
+    for i=1:100
+        push!(data, (x_[i],y_[i]))
+    end
+    return data
+end
+
+
+
+
+
+function main(args=ARGS)
+    s = ArgParseSettings()
+    s.description="optimizers.jl (c) Ozan Arkan Can and Deniz Yuret, 2016. Demonstration of different sgd based optimization methods using LeNet."
+    s.exc_handler=ArgParse.debug_handler
+    @add_arg_table s begin
+        ("--seed"; arg_type=Int; default=-1; help="random number seed: use a nonnegative int for repeatable results")
+        ("--batchsize"; arg_type=Int; default=1; help="minibatch size")
+        ("--lr"; arg_type=Float64; default=0.1; help="learning rate")
+	("--eps"; arg_type=Float64; default=1e-6; help="epsilon parameter used in adam, adagrad, adadelta")
+	("--gamma"; arg_type=Float64; default=0.95; help="gamma parameter used in momentum")
+	("--rho"; arg_type=Float64; default=0.9; help="rho parameter used in adadelta and rmsprop")
+	("--beta1"; arg_type=Float64; default=0.9; help="beta1 parameter used in adam")
+	("--beta2"; arg_type=Float64; default=0.95; help="beta2 parameter used in adam")
+        ("--epochs"; arg_type=Int; default=10; help="number of epochs for training")
+        ("--iters"; arg_type=Int; default=6000; help="number of updates for training")
+	("--optim"; default="Sgd"; help="optimization method (Sgd, Momentum, Adam, Adagrad, Adadelta, Rmsprop)")
+    end
+    println(s.description)
+    isa(args, AbstractString) && (args=split(args))
+    o = parse_args(args, s; as_symbols=true)
+    println("opts=",[(k,v) for (k,v) in o]...)
+    o[:seed] > 0 && srand(o[:seed])
+    gpu() >= 0 || error("LeNet only works on GPU machines.")
+
+    #isdefined(MNIST,:xtrn) || MNIST.loaddata()
+    global dtrn = batch(x_tr, map(diff_sol,x_tr), o[:batchsize])
+    global dtst = batch(x_tst, map(diff_sol,x_tst), o[:batchsize])
+    # dtrn = Any[]
+    # dtst = Any[]
+    # for i=1:10
+    #     push!(dtrn, (x_tr[i],map(diff_sol,x_tr)[i]))
+    #     push!(dtst, (x_tst[i],map(diff_sol,x_tst)[i]))
+    # end
+    # #print("params initiated nad trn, tst pushed")
+    w = init_params()
+    prms = params(w, o)
+
+    # log = Any[]
+    # report(epoch)=push!(log, (:epoch,epoch,:trn,accuracy(w,dtrn,predict),:tst,accuracy(w,dtst,predict)))
+    report(epoch)=println((:epoch,epoch,:trn,accuracy(w,dtrn,predict),:tst,accuracy(w,dtst,predict)))
     report(0)
-    iters = 1000
-    @time for epoch=1:10
-	     train(P, prms, dtrn; epochs=1, iters=iters)
-	      report(epoch)
+    iters = o[:iters]
+    #print(size(w),size(g),size(prms))
+
+    @time for epoch=1:o[:epochs]
+
+	    train(w, prms, dtrn; epochs=10, iters=1000)
+	    report(epoch)
+      # println(w[1][1])
+      # println(w[2][1])
+      # println(w[3][1])
+      # println(w[4][1])
         (iters -= length(dtrn)) <= 0 && break
     end
 
     # for t in log; println(t); end
     return w
 end
+
+main()
---
+++ src/proto/DE-NN-SGD_PaperLoss.jl
@@ -0,0 +1,147 @@
+for p in ("Knet","ArgParse","Compat","GZip")
+    Pkg.installed(p) == nothing && Pkg.add(p)
+end
+
+using Knet,ArgParse,Compat,GZip
+
+
+z(P,i,x) = P[1][i]*x + P[2][i]
+sig_der(x) = sigm(x)*(1-sigm(x))
+
+#Analytical Solution of the Eq
+diff_sol(x) = exp(-(x^2)/2)/(1+x+(x)^3) + x^2
+
+#The phi trial solution
+phi(P,x) = 1 + x*(predict(P,x))
+
+#The Differential Equation function for Example 1
+f(P,x) = x^3 + 2*x + (x^2)*((1+3*(x^2))/(1+x+(x^3))) - (phi(P,x))*(x + ((1+3*(x^2))/(1+x+x^3)))
+
+ x_tr = linspace(0.0,1,10)
+ x_tst = linspace(0.1,1,10)
+
+
+
+
+function predict(P,x)
+  w1, b1, w2 = P
+  h = sigm(w1 * x .+ b1)
+  return w2 * h
+end
+
+function loss(P,x,ygold)
+  dNx = sum([P[3][i]*P[1][i]*sig_der(P[1][i]*x .+ P[2][i]) for i = 1:10])
+  ((predict(P,x)+(x*dNx)-f(P,x))[1])^2
+end
+
+lossgradient = grad(loss)
+
+function train(w, dtrn; lr=.1, epochs=500)
+    for epoch=1:epochs
+        for (x,y) in dtrn
+            g = lossgradient(w, x, y)
+            #println(" pred: ",ypred[1]," orig: ",ygold," error: "ypred-ygold)
+            for i in 1:length(w)
+                # w[i] -= lr * g[i]
+                axpy!(-lr, g[i], w[i])
+            end
+        end
+    end
+    return w
+end
+
+function accuracy(w, dtst, pred=predict)
+    ncorrect = ninstance = nerror = 0
+    for (x, ygold) in dtst
+        ypred = pred(w, x)[1]
+        println(" X: ",x," Prediction: ",ypred[1]," True: ",ygold)
+        nerror += (ygold - ypred[1])
+        ninstance += 1
+    end
+    return (1-(nerror/ninstance), nerror/ninstance)
+end
+
+
+function init_params(;ftype=Float32,atype=KnetArray)
+    #P = Vector{Vector{Float32}}(4)
+    P = Array{Any}(3)
+    P[1] = randn(Float32,10,1)
+    P[2] = zeros(Float32,10,1)
+    P[3] = randn(Float32,1,10)
+    #P[4] = zeros(Float32,1,1)
+    P = map(x -> convert(atype, x), P)
+
+    return P
+end
+
+function batch(x, y, batchsize; atype=Array{Float32})
+    data = Any[]
+    x_ = convert(atype,x)
+    y_ = convert(atype,y)
+    for i=1:10
+        push!(data, (x_[i],y_[i]))
+    end
+    return data
+end
+
+
+
+
+function main(args="")
+    s = ArgParseSettings()
+    s.description="mnist.jl (c) Deniz Yuret, 2016. Multi-layer perceptron model on the MNIST handwritten digit recognition problem from http://yann.lecun.com/exdb/mnist."
+    s.exc_handler=ArgParse.debug_handler
+    @add_arg_table s begin
+        ("--seed"; arg_type=Int; default=-1; help="random number seed: use a nonnegative int for repeatable results")
+        ("--batchsize"; arg_type=Int; default=1; help="minibatch size")
+        ("--epochs"; arg_type=Int; default=500; help="number of epochs for training")
+        ("--hidden"; nargs='*'; arg_type=Int; help="sizes of hidden layers, e.g. --hidden 128 64 for a net with two hidden layers")
+        ("--lr"; arg_type=Float64; default=0.09; help="learning rate")
+        ("--winit"; arg_type=Float64; default=0.1; help="w initialized with winit*randn()")
+        ("--fast"; action=:store_true; help="skip loss printing for faster run")
+        ("--atype"; default=(gpu()>=0 ? "KnetArray{Float32}" : "Array{Float32}"); help="array type: Array for cpu, KnetArray for gpu")
+        ("--gcheck"; arg_type=Int; default=0; help="check N random gradients per parameter")
+        # These are to experiment with sparse arrays
+        # ("--xtype"; help="input array type: defaults to atype")
+        # ("--ytype"; help="output array type: defaults to atype")
+    end
+    isa(args, AbstractString) && (args=split(args))
+    o = parse_args(args, s; as_symbols=true)
+    # if !o[:fast]
+    #     println(s.description)
+    #     println("opts=",[(k,v) for (k,v) in o]...)
+    # end
+    o[:seed] > 0 && srand(o[:seed])
+    atype = eval(parse(o[:atype]))
+    w = init_params()
+    #w = weights(o[:hidden]...; atype=atype, winit=o[:winit])
+    #if !isdefined(MNIST,:xtrn); loaddata(); end
+
+    global dtrn = batch(x_tr, map(diff_sol,x_tr), o[:batchsize])
+    global dtst = batch(x_tst, map(diff_sol,x_tst), o[:batchsize])
+    report(epoch)=println((:epoch,epoch,:trn,accuracy(w,dtrn),:tst,accuracy(w,dtst)))
+    if o[:fast]
+        (train(w, dtrn; lr=o[:lr], epochs=o[:epochs]); gpu()>=0 && Knet.cudaDeviceSynchronize())
+    else
+        report(0)
+        @time for epoch=1:o[:epochs]
+            train(w, dtrn; lr=o[:lr], epochs=500)
+            report(epoch)
+            if o[:gcheck] > 0
+                gradcheck(loss, w, first(dtrn)...; gcheck=o[:gcheck], verbose=true)
+            end
+        end
+    end
+    return w
+end
+
+# This allows both non-interactive (shell command) and interactive calls like:
+# $ julia mnist.jl --epochs 10
+# julia> MNIST.main("--epochs 10")
+if VERSION >= v"0.5.0-dev+7720"
+    PROGRAM_FILE == "mnist.jl" && main(ARGS)
+else
+    !isinteractive() && !isdefined(Core.Main,:load_only) && main(ARGS)
+end
+
+main()
