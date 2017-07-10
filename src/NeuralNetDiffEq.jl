a7b3498999776d8baf3216c5c7b9ff7a6f2ff2ec
akaysh <ksh.shrm1@gmail.com>
2017-07-10 18:17:30 +0530

    Cleaned Comments, names and variables

--- src/NeuralNetDiffEq.jl
+++ src/NeuralNetDiffEq.jl
@@ -16,14 +16,12 @@ end
 
 
 nnode(;hl_width=10) = nnode(hl_width)
-#nnode(hl_width::Integer) = nnode([hl_width])
 
 
 
 export nnode
 
 #constant functions
-#z(P,i,x) = P[1][i]*x + P[2][i]
 sig_der(x) = sigm(x)*(1-sigm(x))
 
 
@@ -53,11 +51,10 @@ function solve(
     #hidden layer(s)
     hl_width = alg.hl_width
 
-    #The phi trial solution
-    trial_funcs = Array{Function}(outdim)
+    #The trial solutions (one for each NN or ODE)
+    trial_solutions = Array{Function}(outdim)
     for i = 1:outdim
-        phi(P,t) = u0[i] + (t .- t0).*predict(P,t)[1]
-        trial_funcs[i] = phi
+        trial_solutions[i](P,t) = u0[i] + (t .- t0).*predict(P,t)[1]
     end
 
     #train points generation
@@ -67,40 +64,38 @@ function solve(
     _maxiters = iterations
 
     #initialization of weights and bias
-    NNs = Array{Any}(outdim)
+    NNs = Array{Any}(outdim) #Array of Neural Nets each with w1, b1 and w2
     for i = 1:outdim
-        w = init_params(uElType,hl_width)
-        NNs[i] = w
+        NNs[i] = init_weights_and_biases(uElType,hl_width)
     end
-    #w = init_params(uElType,hl_width)
 
     #initialization of optimization parameters (Adam by default for now)
-    lr_ = 0.06
+    lr_ = 0.5
     beta1_ = 0.9
     beta2_ = 0.95
     eps_ = 1e-6
     prms = Any[]
     Params = Array{Any}(outdim)
     for i=1:length(NNs[1])
-        prm = Adam(lr=lr_, beta1=beta1_, beta2=beta2_, eps=eps_)
-        #prm = Sgd(;lr=lr_)
+        #prm = Adam(lr=lr_, beta1=beta1_, beta2=beta2_, eps=eps_)
+        prm = Sgd(;lr=lr_)
         push!(prms, prm)
     end
 
     for i=1:length(NNs)
         Params[i] = copy(prms)
     end
-    #println(length(NNs),outdim)
+
+    println(NNs)
 
     @time for iters=1:_maxiters
-            train(NNs, Params, dtrn, f, trial_funcs, hl_width; maxiters=1)
-            #losses = [loss_trial(NNs[i],dtrn,f,trial_funcs,hl_width) for i = 1:length(trial_funcs)]
-            loss = loss_trial(NNs,dtrn,f,trial_funcs,hl_width)
+            train(NNs, Params, dtrn, f, trial_solutions, hl_width; maxiters=1)
+            println(NNs)
+
+            loss = loss_trial(NNs,dtrn,f,trial_solutions,hl_width)
             if mod(iters,100) == 0
                 println((:iteration,iters,:loss,loss))
             end
-            #gradcheck(loss_trial, w, dtrn, f, phi, hl_width...; gcheck=10, verbose=true)
-            #check_Phi_gradient(w,dtrn,hl_width)
             if maximum(loss) < 10^(-15.0)
                 print(:loss,loss)
                 break
@@ -109,7 +104,7 @@ function solve(
 
 
     #solutions at timepoints
-    u = [get_trial_sols(trial_funcs,NNs,x) for x in dtrn]
+    u = [get_trial_sol_values(trial_solutions,NNs,x) for x in dtrn]
 
 
     build_solution(prob,alg,dtrn,u,
--- src/training_utils.jl
+++ src/training_utils.jl
@@ -1,45 +1,38 @@
-
+#Utility functions for solver
 
 function predict(P,x)
     w1, b1, w2 = P
-    h = sigm(w1 * x .+ b1)
-    return w2 * h
+    return w2*sigm(w1 * x .+ b1)
 end
 
-function get_trial_sols(trial_funcs,NNs,t)
-    trial_sols = Array{Any}(length(NNs))
+function get_trial_sol_values(trial_solutions,NNs,t)
+    trial_sol_values = Array{Any}(length(NNs))
     for i = 1:length(NNs)
-        trial_sols[i] = trial_funcs[i](NNs[i],t)
+        trial_sol_values[i] = trial_solutions[i](NNs[i],t)
     end
-    trial_sols
+    trial_sol_values
 end
 
-function loss_trial(NNs,timepoints,f,trial_funcs,hl_width)
-    sum([sumabs2([gradient(x->trial_funcs[i](NNs[i],x),t) .- f(t,[trial_func(NNs[i],t) for trial_func in trial_funcs])[i]  for t in timepoints]) for i =1:length(NNs)])
+function loss_trial(NNs,timepoints,f,trial_solutions,hl_width)
+    sum([sumabs2([gradient(x->trial_solutions[i](NNs[i],x),t) .- f(t,[trial_func(NNs[i],t) for trial_func in trial_solutions])[i]  for t in timepoints]) for i =1:length(NNs)])
 end
 
 lossgradient = grad(loss_trial)
 
-function train(NNs, prms, timepoints, f, trial_funcs, hl_width; maxiters =1)
+function train(NNs, prms, timepoints, f, trial_solutions, hl_width; maxiters =1)
         for x in timepoints
-            #for i = 1:length(NNs)
-                g = lossgradient(NNs,timepoints,f,trial_funcs,hl_width)
+                g = lossgradient(NNs,timepoints,f,trial_solutions,hl_width)
                 update!(NNs, g, prms)
-            #end
         end
     return NNs
 end
 
 
-function init_params(ftype,hl_width;atype=KnetArray{Float32})
-    #P = Vector{Vector{Float32}}(4)
-    P = Array{Any}(3)
+function init_weights_and_biases(ftype,hl_width;atype=KnetArray{Float32})
+    P = Array{Any}(3) #Constant layers and parameters for now
     P[1] = randn(ftype,hl_width,1)*(0.01^2)  #To reduce variance
     P[2] = zeros(ftype,hl_width,1)
     P[3] = randn(ftype,1,hl_width)*(0.01^2)  #To reduce variance
-    #P[4] = zeros(Float32,1,1)
-    #P = map(x -> convert(atype, x), P)
-
     return P
 end
 
