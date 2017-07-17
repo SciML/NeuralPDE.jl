d920d5d32d005bfd67709e55030473568c818dc2
akaysh <ksh.shrm1@gmail.com>
2017-07-18 01:36:25 +0530

    Interpolation modifications

--- src/NeuralNetDiffEq.jl
+++ src/NeuralNetDiffEq.jl
@@ -14,31 +14,38 @@ immutable nnode <: NeuralNetDiffEqAlgorithm
 end
 
 
-
 nnode(;hl_width=10) = nnode(hl_width)
 
-
-
 export nnode
+
+
+immutable NeuralNetworkInterpolation{T1,T2}
+  trial_solutions::T1
+  NNs::T2
+end
+
+(id::NeuralNetworkInterpolation)(t,idxs,deriv) = NN_interpolation(t,id,idxs,deriv)
 
 #constant functions
 sig_der(x) = sigm(x)*(1-sigm(x))
 
 
-function NN_interpolation(t,idxs,deriv)
+function NN_interpolation(t,id,idxs,deriv)
+
     if idxs != nothing || deriv != Val{0}
         error("No use of idxs and derivative in single ODE")
     end
 
-    return [get_trial_sol_values(trial_solutions,NNs,t)]
+    return get_trial_sol_values(id.trial_solutions,id.NNs,t)
 end
 
-(sol::ODESolution)(t,deriv::Type=Val{0};idxs=nothing) = sol.interp(t,idxs,deriv)
+
 
 function solve(
     prob::AbstractODEProblem,
     alg::NeuralNetDiffEqAlgorithm;
     dt = nothing,
+    dense = true,
     timeseries_errors = true,
     iterations = 50,
     kwargs...)
@@ -62,7 +69,7 @@ function solve(
     hl_width = alg.hl_width
 
     #The trial solutions (one for each NN or ODE)
-    global trial_solutions = Array{Function}(outdim)
+    trial_solutions = Array{Function}(outdim)
     for i = 1:outdim
         u(P,t) = u0[i] + (t .- t0).*predict(P,t)[1]
         trial_solutions[i] = u
@@ -75,7 +82,7 @@ function solve(
     _maxiters = iterations
 
     #initialization of weights and bias
-    global NNs = Array{Any}(outdim) #Array of Neural Nets each with w1, b1 and w2
+    NNs = Array{Any}(outdim) #Array of Neural Nets each with w1, b1 and w2
     for i = 1:outdim
         NNs[i] = init_weights_and_biases(uElType,hl_width)
     end
@@ -117,8 +124,9 @@ function solve(
 
 
     build_solution(prob,alg,dtrn,u,
+               dense = dense,
                timeseries_errors = timeseries_errors,
-               interp = NN_interpolation,
+               interp = NeuralNetworkInterpolation(trial_solutions,NNs),
                retcode = :Success)
 
 end #solve
