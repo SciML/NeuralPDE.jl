8e96ad12c4c82864aa1abe8e4f69983df5607a4a
KirillZubov <kirillzubov3@gmail.com>
2019-06-19 00:40:19 +0300

    fix a few issues

--- src/solve.jl
+++ src/solve.jl
@@ -18,36 +18,30 @@ function DiffEqBase.solve(
     #hidden layer
     chain  = alg.chain
     opt    = alg.opt
-    ps     = Flux.Params(chain)
+    ps     = Flux.params(chain)
     data   = Iterators.repeated((), maxiters)
 
     #train points generation
     ts = tspan[1]:dt:tspan[2]
 
     #The phi trial solution
-    phi(t) = u0 .+ x.*chain([t])
+    phi(t) = u0 .+ t.*chain(Tracker.collect([t]))
 
     #derivatives of a function f
-    dfdx(t) = Tracker.gradient(t -> chain([t]), t; nest = true)
+    dfdx(t) = Tracker.gradient(t -> sum(phi(t)), t; nest = true)[1]
 
     #loss function for training
-    loss() = sum(abs2, dfdx(t) - f(phi(t),p,t) for t in ts)
+    loss() = sum(abs2, dfdx(t) - f(phi(t)[1],p,t) for t in ts)
 
-    @time for iters=1:maxiters
-        Flux.train!(loss, ps, data, opt)
-        if mod(iters,50) == 0
-            loss_value = loss().data
-            println((:iteration,iters,:loss,loss_value))
-            if loss_value < abstol
-                break
-            end
-        end
+    cb = function ()
+        loss() < abstol && Flux.stop()
     end
+    Flux.train!(loss, ps, data, opt; cb = cb)
 
     #solutions at timepoints
-    u = [phi(t).data for t in ts]
+    u = [phi(t)[1].data for t in ts]
 
-    sol = DiffEqBase.build_solution(prob,alg,x,u,calculate_error = false)
+    sol = DiffEqBase.build_solution(prob,alg,ts,u,calculate_error = false)
     DiffEqBase.has_analytic(prob.f) && DiffEqBase.calculate_solution_errors!(sol;timeseries_errors=true,dense_errors=false)
     sol
 end #solve
