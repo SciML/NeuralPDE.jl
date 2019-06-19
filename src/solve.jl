6a9502a7e9bbccc336acb05ec4c89a8f2ef160ca
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-06-18 20:46:01 -0400

    Array-based

--- src/solve.jl
+++ src/solve.jl
@@ -7,6 +7,7 @@ function DiffEqBase.solve(
     save_everystep=true,
     adaptive=false,
     abstol = 1f-6,
+    verbose = false,
     maxiters = 100)
 
     u0 = prob.u0
@@ -25,21 +26,25 @@ function DiffEqBase.solve(
     ts = tspan[1]:dt:tspan[2]
 
     #The phi trial solution
-    phi(t) = u0 .+ t.*chain(Tracker.collect([t]))
+    phi(t) = u0 .+ (t .- tspan[1]).*chain(Tracker.collect([t]))
 
     #derivatives of a function f
-    dfdx(t) = Tracker.gradient(t -> sum(phi(t)), t; nest = true)[1]
+    dfdx(t) = (phi(t+sqrt(eps(typeof(dt)))) - phi(t)) / sqrt(eps(typeof(dt)))
+    #dfdx(t) = Flux.Tracker.forwarddiff(phi,t)
+    #dfdx(t) = Tracker.collect([Flux.Tracker.gradient(t->phi(t)[i],t, nest=true) for i in 1:length(u0)])
 
     #loss function for training
-    loss() = sum(abs2, dfdx(t) - f(phi(t)[1],p,t) for t in ts)
+    loss() = sum(abs2,sum(abs2,dfdx(t) - f(phi(t),p,t)) for t in ts)
 
     cb = function ()
-        loss() < abstol && Flux.stop()
+        l = loss()
+        verbose && println("Current loss is: $l")
+        l < abstol && Flux.stop()
     end
     Flux.train!(loss, ps, data, opt; cb = cb)
 
     #solutions at timepoints
-    u = [phi(t)[1].data for t in ts]
+    u = [phi(t).data for t in ts]
 
     sol = DiffEqBase.build_solution(prob,alg,ts,u,calculate_error = false)
     DiffEqBase.has_analytic(prob.f) && DiffEqBase.calculate_solution_errors!(sol;timeseries_errors=true,dense_errors=false)
