b7586494c996dded5eb544b0b13d8d1bcdc2242d
KirillZubov <kirillzubov3@gmail.com>
2019-07-31 18:04:23 +0300

    fix tracked σᵀ∇u, u0

--- src/neural_sde.jl
+++ src/neural_sde.jl
@@ -1,15 +1,15 @@
-using Flux, DiffEqFlux, StochasticDiffEq,LinearAlgebra
+using Flux, DiffEqFlux, StochasticDiffEq, LinearAlgebra
+
 d = 3
 hls = 10 + d
-# _u0 = Flux.data(u0(x0)[1])
-
 datasize = 5
 tspan = (0.0f0,1.0f0)
 t = range(tspan[1],tspan[2],length=datasize)
-
-f(X, u, _σᵀ∇u, p, t) = 0.5 * (u .- sum(X.*_σᵀ∇u))
+x0 = Float32[1.,1.,1.]
+f(X, u, _σᵀ∇u, p, t) = Float32(0.05) * (u .- sum(X.*_σᵀ∇u))
 μ(X,p,t) = 1.0
 σ(X,p,t) = 1.0
+g(X) = sum(X.^2)
 
 u0 = Flux.Chain(Dense(d,hls,relu),
                 Dense(hls,hls,relu),
@@ -18,62 +18,66 @@ u0 = Flux.Chain(Dense(d,hls,relu),
 σᵀ∇u = Flux.Chain(Dense(d,hls,relu),
                   Dense(hls,hls,relu),
                   Dense(hls,d))
-g(X) = sum(X.^2)
 
-function trueODEfunc(dh,h,p,t)
+
+function F(h,p,t)
     u =  h[end]
     X =  h[1:end-1]
-    _σᵀ∇u = σᵀ∇u(X) #TODO tracked σᵀ∇u
-    dh[end] = Flux.data(f(X, u, _σᵀ∇u, p, t))
-    dh[1:end-1] .= μ(X,p,t)
-    dh
+    _σᵀ∇u = σᵀ∇u(X) #TODO time dependence
+    _f = -f(X, u, _σᵀ∇u, p, t)
+    Flux.Tracker.collect([Float32[μ(x,p,t) for x in X]; [_f]])
 end
 
-function true_noise_func(dh,h,p,t)
+function G(h,p,t)
     X = h[1:end-1]
-    _σᵀ∇u = Flux.data(σᵀ∇u(X))
-    dh[end] = sum(_σᵀ∇u) # TODO _σᵀ∇u*dW nondioganal
-    dh[1:end-1] .= σ(X,p,t)
-    dh
+    _σᵀ∇u = σᵀ∇u(h[1:end-1]) #TODO time dependence
+    Flux.Tracker.collect([Float32[σ(x,p,t) for x in x0]; [sum(_σᵀ∇u)]]) ## TODO _σᵀ∇u*dW nondioganal
 end
 
 # prob = SDEProblem(trueODEfunc,true_noise_func,init_cond,tspan) #,noise_rate_prototype=zeros(2,d)
 # sol = solve(prob,SRIW1())
 # println(sol)
 
-function neural_sde(init_cond, trueODEfunc, true_noise_func, tspan, args...; kwargs...)
-    prob = SDEProblem(trueODEfunc,true_noise_func, init_cond, tspan,nothing) #,noise_rate_prototype=zeros(2,d)
+function neural_sde(init_cond, F, G, tspan, args...; kwargs...)
+    prob = SDEProblem(F,G, init_cond, tspan,nothing) #noise_rate_prototype=zeros(2,d)
     solve(prob, args...; kwargs...) |> Tracker.collect
 end
 
-n_sde = init_cond->neural_sde(init_cond,trueODEfunc,true_noise_func,tspan,SOSRI(),saveat=t,reltol=1e-1,abstol=1e-1)
-
-x0 = fill(1.0 , d)
-init_cond = fill(1.0 , d+1)
+n_sde = init_cond->neural_sde(init_cond,F,G,tspan,SOSRI(),saveat=t,reltol=1e-1,abstol=1e-1)
 
 function predict_n_sde()
-    _u0 = Flux.data(u0(x0)[1])
-    init_cond[d+1] =_u0 #TODO tracked u0
-    ans = n_sde(init_cond)
+    _u0 = u0(x0)
+    init_cond = [x0;_u0]
+    n_sde(init_cond)
 end
 
+# init_cond = [Flux.data(_u0),Flux.data(_u0),Flux.data(_u0),Flux.data(_u0)]
+# prob = ODEProblem(dudt_,x,tspan,p)
+# prob = SDEProblem(trueODEfunc,true_noise_func, init_cond, tspan,nothing)
+# diffeq_rd(p,prob,Tsit5())
+# _init_cond = vcat(param(Float32[1., 1., 1.]),_u0)
+# function predict_n_sde()
+#     _u0 = u0(x0)
+#     _init_cond = vcat(x0,_u0)
+#     Flux.Tracker.collect(diffeq_rd(1,prob,SOSRI(),u0=_init_cond,saveat=t,reltol=1e-1,abstol=1e-1))
+# end
+
+# ans = predict_n_sde()
+# println(ans)
 # ans = predict_n_sde()[:,end]
 # (X, u) = (ans[1:end-1],ans[end])
 # println(ans)
 # println((X, u))
 
 function loss_n_sde()
-    ans = predict_n_sde()[:,end]
-    (X, u) = (ans[1:end-1],ans[end])
+    predict_ans = predict_n_sde()[:,end]
+    (X, u) = (Flux.data(predict_ans[1:end-1]), predict_ans[end])
     sum(abs2,g(X) - u)
 end
 
-
 ps = Flux.params(u0, σᵀ∇u)
-
-opt = ADAM(0.025)
-data = Iterators.repeated((), 100)
-
+opt = ADAM(0.005)
+data = Iterators.repeated((), 50)
 verbose = true
 abstol=1e-8
 
