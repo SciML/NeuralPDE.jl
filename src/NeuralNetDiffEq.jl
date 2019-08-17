7136ad6a080554b9b96249214c8b9a8cd13cc671
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-08-17 07:38:21 -0400

    Update NeuralNetDiffEq.jl

--- src/NeuralNetDiffEq.jl
+++ src/NeuralNetDiffEq.jl
@@ -21,6 +21,14 @@ struct TerminalPDEProblem{G,F,Mu,Sigma,X,T,P} <: DiffEqBase.DEProblem
                                                          g,f,μ,σ,X0,tspan,p)
 end
 
+Base.summary(prob::TerminalPDEProblem) = string(nameof(typeof(prob)))
+
+function Base.show(io::IO, A::TerminalPDEProblem)
+  println(io,summary(A))
+  print(io,"timespan: ")
+  show(io,A.tspan)
+end
+
 include("ode_solve.jl")
 include("pde_solve.jl")
 include("pde_solve_ns.jl")
