4b78757be0a884667acfca9bc408772af4c28f71
Rayhan Razzak <rayhanrazzak@Rayhans-MBP.attlocal.net>
2019-12-12 15:10:47 -0800

    making a fork for second order ODE

--- src/ode_solve.jl
+++ src/ode_solve.jl
@@ -1,3 +1,5 @@
+using Flux.Tracker
+
 struct NNODE{C,O} <: NeuralNetDiffEqAlgorithm
     chain::C
     opt::O
@@ -9,6 +11,7 @@ function DiffEqBase.solve(
     alg::NeuralNetDiffEqAlgorithm,
     args...;
     dt,
+    initial_condition,
     timeseries_errors = true,
     save_everystep=true,
     adaptive=false,
@@ -18,7 +21,9 @@ function DiffEqBase.solve(
 
     DiffEqBase.isinplace(prob) && error("Only out-of-place methods are allowed!")
 
-    u0 = prob.u0
+    u1 = initial_condition[1]
+    u2 = 0.0f0
+
     tspan = prob.tspan
     f = prob.f
     p = prob.p
@@ -34,17 +39,28 @@ function DiffEqBase.solve(
     ts = tspan[1]:dt:tspan[2]
 
     #The phi trial solution
-    phi(t) = u0 .+ (t .- tspan[1]).*chain(Tracker.collect([t]))
+    phi(t) = u1 .+ (t .- tspan[1]).*chain(Tracker.collect([t]))
+    if(length(initial_condition) == 2)
+        u2 = initial_condition[2]
+        phi(t) = (u1 .* (1 - (t .- tspan[1]))) .+ (u2 .* (t .- tspan[1])) .+ ((t .- tspan[1]) .* (1 - (t .- tspan[1])) .* chain(Tracker.collect([t])))
+    end
 
-    if u0 isa Number
+    if u1 isa Number && length(initial_condition) != 2
         dfdx = t -> Tracker.gradient(t -> sum(phi(t)), t; nest = true)[1]
         loss = () -> sum(abs2,sum(abs2,dfdx(t) .- f(phi(t)[1],p,t)[1]) for t in ts)
     else
         dfdx = t -> (phi(t+sqrt(eps(typeof(dt)))) - phi(t)) / sqrt(eps(typeof(dt)))
         #dfdx(t) = Flux.Tracker.forwarddiff(phi,t)
-        #dfdx(t) = Tracker.collect([Flux.Tracker.gradient(t->phi(t)[i],t, nest=true) for i in 1:length(u0)])
+        #dfdx(t) = Tracker.collect([Flux.Tracker.gradient(t->phi(t)[i],t, nest=true) for i in 1:length(u1)])
         #loss function for training
         loss = () -> sum(abs2,sum(abs2,dfdx(t) - f(phi(t),p,t)) for t in ts)
+    end
+
+    if length(initial_condition) == 2
+        dfdx = t -> (phi(t+sqrt(eps(typeof(dt)))) - phi(t)) / sqrt(eps(typeof(dt)))
+        df2dx = j -> Tracker.gradient(j -> sum(dfdx(j)), j; nest = true)[1]
+        #df2dx = t -> Tracker.gradient(t -> sum(dfdx(t)), t; nest = true)[1]
+        loss = () -> sum(abs2,sum(abs2,df2dx(t) .- f(phi(t)[1],p,t)[1]) for t in ts)
     end
 
     cb = function ()
@@ -55,7 +71,7 @@ function DiffEqBase.solve(
     Flux.train!(loss, ps, data, opt; cb = cb)
 
     #solutions at timepoints
-    if u0 isa Number
+    if u1 isa Number
         u = [phi(t)[1].data for t in ts]
     else
         u = [phi(t).data for t in ts]
--- test/NNODE_tests.jl
+++ test/NNODE_tests.jl
@@ -1,15 +1,56 @@
-using Test, Flux, NeuralNetDiffEq
+ENV["GKS_ENCODING"]="utf-8"
+
+using Test, Flux, NeuralNetDiffEq, Plots, Plots.PlotMeasures
 using DiffEqDevTools
 
-# Run a solve on scalars
-linear = (u,p,t) -> cos(2pi*t)
+
+# Run a solve on vectors
+
+linear = (u,p,t) -> [cos(2pi*t)]
 tspan = (0.0f0, 1.0f0)
 u0 = 0.0f0
 prob = ODEProblem(linear, u0 ,tspan)
 chain = Flux.Chain(Dense(1,5,σ),Dense(5,1))
 opt = Flux.ADAM(0.1, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNODE(chain,opt), dt=1/20f0, verbose = true,
+sol = solve(prob, NeuralNetDiffEq.NNODE(chain,opt), initial_condition = [u0], dt=1/20f0, abstol=1e-10,
+           verbose = true, maxiters=200)
+
+
+# Second-Order ODE - Dirichlet
+linear = (u,p,t) -> cos(2pi*t)
+tspan = (0.0f0, 1.0f0)
+u0 = 0.0f0
+u1 = 1.0f0
+initial_condition = [u0, u1]
+prob = ODEProblem(linear, u0, tspan)
+
+chain = Flux.Chain(Dense(1,5,σ),Dense(5,1))
+opt = Flux.ADAM(0.1, (0.9, 0.95))
+sol = solve(prob, NeuralNetDiffEq.NNODE(chain,opt), initial_condition = [u0, u1], dt=1/20f0, verbose = true,
             abstol=1e-10, maxiters = 200)
+
+analytical_sol = t -> ((4pi^2 * t) - cos(2pi*t) + 1) / (4 * pi^2)
+plot(analytical_sol, label = "analytical", linestyle = :dot, linewidth = 8, legend=:bottomright, bottom_margin = 10mm, left_margin=10mm, top_margin=10mm, right_margin=10mm)
+plot!(sol, title = "Second-Order Dirichlet : Cos(2𝝅t)", label = "phi", fillalpha = .5, linewidth = 4)
+xlabel!("Time (t)")
+display(ylabel!("f(x)", ylims = (0,1)))
+savefig("plot.png")
+
+#=
+anat_sum = zeros(0)
+append!(anat_sum, 0)
+for i = 1:20
+    k = i * .05
+    append!(anat_sum, analytical_sol(k))
+end
+
+print("length of sol is: ", length(sol))
+print("length of anat_sum is:", length(anat_sum))
+#error = sol .- analytical_sol
+#print("Error: ", error)
+#@test error < 0.5
+
+=#
 
 # Run a solve on vectors
 linear = (u,p,t) -> [cos(2pi*t)]
@@ -18,7 +59,7 @@ u0 = [0.0f0]
 prob = ODEProblem(linear, u0 ,tspan)
 chain = Flux.Chain(Dense(1,5,σ),Dense(5,1))
 opt = Flux.ADAM(0.1, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNODE(chain,opt), dt=1/20f0, abstol=1e-10,
+sol = solve(prob, NeuralNetDiffEq.NNODE(chain,opt), initial_condition = [u0], dt=1/20f0, abstol=1e-10,
             verbose = true, maxiters=200)
 
 #Example 1
@@ -27,9 +68,9 @@ linear_analytic = (u0,p,t) -> [exp(-(t^2)/2)/(1+t+t^3) + t^2]
 prob = ODEProblem(ODEFunction(linear,analytic=linear_analytic),[1f0],(0.0f0,1.0f0))
 chain = Flux.Chain(Dense(1,5,σ),Dense(5,1))
 opt = Flux.ADAM(0.1, (0.9, 0.95))
-sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),verbose = true, dt=1/5f0)
+sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),initial_condition = [prob.u0], verbose = true, dt=1/5f0)
 err = sol.errors[:l2]
-sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),verbose = true, dt=1/20f0)
+sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),initial_condition = [prob.u0], verbose = true, dt=1/20f0)
 sol.errors[:l2]/err < 0.5
 
 #=
@@ -45,9 +86,9 @@ linear_analytic = (u0,p,t) ->  exp(-t/5)*(u0 + sin(t))
 prob = ODEProblem(ODEFunction(linear,analytic=linear_analytic),0.0f0,(0.0f0,1.0f0))
 chain = Flux.Chain(Dense(1,5,σ),Dense(5,1))
 opt = Flux.ADAM(0.1, (0.9, 0.95))
-sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),verbose = true, dt=1/5f0)
+sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt), initial_condition = [prob.u0], verbose = true, dt=1/5f0)
 err = sol.errors[:l2]
-sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),verbose = true, dt=1/20f0)
+sol  = solve(prob,NeuralNetDiffEq.NNODE(chain,opt),initial_condition = [prob.u0], verbose = true, dt=1/20f0)
 sol.errors[:l2]/err < 0.5
 
 #=
