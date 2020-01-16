51fd4acc453498f16d8c5f4a147e9f98fb136044
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-01-16 07:54:27 -0500

    split tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
 using SafeTestsets
 
+const GROUP = get(ENV, "GROUP", "All")
+const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
+const is_TRAVIS = haskey(ENV,"TRAVIS")
+
 @time begin
-@time @safetestset "NNODE" begin include("NNODE_tests.jl") end
-@time @safetestset "NNPDEHan" begin include("NNPDEHan_tests.jl") end
-@time @safetestset "NNPDENS" begin include("NNPDENS_tests.jl") end
+if GROUP == "All" || GROUP == "Test1"
+    @time @safetestset "NNODE" begin include("NNODE_tests.jl") end
+    @time @safetestset "NNPDEHan" begin include("NNPDEHan_tests.jl") end
+end
+if GROUP == "All" || GROUP == "Test2"
+    @time @safetestset "NNPDENS" begin include("NNPDENS_tests.jl") end
+end
 end
