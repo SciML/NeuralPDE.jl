1a24c4f0a785f1348d2c6ba1cf82e5f5e93be37c
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-24 15:39:37 +0800

    Update NNODE2_tests.jl

--- test/NNODE2_tests.jl
+++ test/NNODE2_tests.jl
@@ -13,16 +13,9 @@ chain = Chain(Dense(1,5,σ),Dense(5,1))
 opt = ADAM(0.1, (0.9, 0.95))
 sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true, abstol=1e-10, maxiters = 200)
 
-function an_sol(tspan, dt)
-    ans = []
-    cnt = 0.0
-    for i in 1:Int(tspan[2]/dt)+1
-        push!(ans, 1/5 * exp(-3*cnt) *(3*exp(5*cnt)+2.0))
-        cnt += dt
-    end
-    return ans
-end
+t = tspan[1]:dt:tspan[2]
+an_sol = @. 1/5 * exp(-3*t) *(3*exp(5*t)+2.0)
 
-plot(an_sol(tspan, dt), sol, title="Second Order Linear ODE", label=["Analytical","Numerical"], lw=3)
+plot(an_sol, sol, title="Second Order Linear ODE", label=["Analytical","Numerical"], lw=3)
 xlabel!("t")
 ylabel!("y(t)")
