c2c686e915c26fdbe0e95a4ff1a4cfc14bea4372
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-24 16:14:45 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ const is_TRAVIS = haskey(ENV,"TRAVIS")
 if GROUP == "All" || GROUP == "Test1"
     @time @safetestset "NNODE" begin include("NNODE_tests.jl") end
     @time @safetestset "NNPDEHan" begin include("NNPDEHan_tests.jl") end
+    @time @safetestset "NNODE2" begin include("NNODE2_tests.jl") end
 end
 if GROUP == "All" || GROUP == "Test2"
     @time @safetestset "NNPDENS" begin include("NNPDENS_tests.jl") end
