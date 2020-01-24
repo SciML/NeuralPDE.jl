2d10dc9b72f85105c519af610d6b0363638a09f8
CCL <ccl5a09@gamail.com>
2020-01-24 12:11:54 +0800

    https://github.com/chinglamchoi/NeuralNetDiffEq.jl

--- test/NNODE2_tests.jl
+++ test/NNODE2_tests.jl
@@ -1,4 +1,4 @@
-using Test, Flux, NeuralNetDiffEq
+using Test, Flux
 using Plots
 using DiffEqDevTools
 
@@ -7,12 +7,11 @@ f(du, u, p, t) = -du+6*u
 tspan = (0.0f0, 2.0f0)
 u0 = [1.0f0]
 du0 = [0.0f0]
-dt = 1/20f0
+dt = [1/20f0]
 prob = SecondOrderODEProblem(f, u0, du0, tspan)
 chain = Chain(Dense(1,5,σ),Dense(5,1))
 opt = ADAM(0.1, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true,
-            abstol=1e-10, maxiters = 200)
+sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true, abstol=1e-10, maxiters = 200)
 
 function an_sol(tspan, dt)
     ans = []
@@ -27,4 +26,3 @@ end
 plot(an_sol(tspan, dt), sol, title="Second Order Linear ODE", label=["Analytical","Numerical"], lw=3)
 xlabel!("t")
 ylabel!("y(t)")
-
