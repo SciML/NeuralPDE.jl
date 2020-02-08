0946a63673ad8fb7a6ea8eb643b472138b298a41
Ching Lam Choi <ccl5a09@gmail.com>
2020-02-08 21:14:39 +0800

    Update second_ode_solve.jl

--- src/second_ode_solve.jl
+++ src/second_ode_solve.jl
@@ -29,18 +29,19 @@ function DiffEqBase.solve(
     data   = Iterators.repeated((), maxiters)
 
     #train points generation
-    cnt = 0.0f0
     ts  = tspan[1]:dt:tspan[2]
     
     #Initial Value Problem
-    phi(t) = (u0 .+ du0 .* (t .- tspan[1]) .+ (t .- tspan[1]) .^ 2 .* chain([t]))[1]
-
-
-    dfdx = t -> (phi(t+sqrt(eps(typeof(dt)))) - phi(t)) / sqrt(eps(typeof(dt)))
+    if u0 isa Number && du0 isa Number
+        phi(t) = (u0 + du0 * (t - tspan[1]) + (t - tspan[1]) ^ 2 .* chain([t]))[1]
+    else
+        phi(t) = (u0 .+ du0 .* (t - tspan[1]) .+ (t - tspan[1]) .^ 2 .* chain([t]))
+    end
+    dfdx = t -> (phi(t+sqrt(eps(typeof(dt)))) .- phi(t)) / sqrt(eps(typeof(dt)))
     epsilon(t) = cbrt(eps(typeof(dt)))
     #second order central
-    d2fdx2(t) = (phi(t+epsilon(t)) - 2phi(t) + phi(t-epsilon(t)))/epsilon(t)^2
-    loss = () -> sum(abs2,sum(abs2,d2fdx2(t) .- f(dfdx(t), phi(t),p,t)[1]) for t in ts)
+    d2fdx2(t) = (phi(t+epsilon(t)) - 2phi(t) .+ phi(t-epsilon(t)))/epsilon(t)^2
+    loss = () -> sum(abs2,sum(abs2,d2fdx2(t) .- f(dfdx(t), phi(t),p,t)) for t in ts) #[1] after f()
 
     cb = function ()
         l = loss()
