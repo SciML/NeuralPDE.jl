5bf7ad80b6bd7ce46a1622d8a2925cf1d370f406
Ching Lam Choi <ccl5a09@gmail.com>
2020-02-08 21:15:40 +0800

    Update NNODE2_tests2.jl

--- test/NNODE2_tests2.jl
+++ test/NNODE2_tests2.jl
@@ -1,9 +1,9 @@
+#using Plots
 using Test, Flux
-using Plots
 using DiffEqDevTools
-using DiffEqBase, Main.NeuralNetDiffEq
+using DiffEqBase, NeuralNetDiffEq
 
-function f(ddu, du, u, p, t)
+function f(ddu, du, u, p, t) #maybe turn to arrays too
 	ddu[1] = (-p[3]*u[1]-p[4]*(u[1]-u[2]))/p[1]
 	ddu[2] = (-p[4]*(u[2]-u[1]))/p[2]
 end
@@ -13,9 +13,11 @@ end
 p = [1.0f0, 1.0f0, 6.0f0, 4.0f0]
 u0 = [1.0f0, 0.0f0]
 du0 = [2.0f0, 0.0f0]
-prob = SecondOrderODEProblem{false}(f, u0, du0, (0.0f0, 3.0f0), p)
+prob = SecondOrderODEProblem{false}(f, [u0], [du0], (0.0f0, 3.0f0), p)
 dt = 1/20f0
 opt = ADAM(1e-03)
+chain = Chain(Dense(1,5,σ),Dense(5,1))
+#=
 chain = Chain(
     x -> reshape(x, length(x), 1, 1), 
     MaxPool((1,)), 
@@ -33,5 +35,6 @@ chain = Chain(
     Dense(128, 64, tanh), 
     Dense(64, 2), 
     softmax)
+=#
 sol = solve(prob, NeuralNetDiffEq.NNODE2(chain, opt), dt=dt, verbose=true, abstol=1e-10, maxiters=50)
-plot(sol)
+#plot(sol)
