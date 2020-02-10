aa819a85494dbe06adcfdb0051fd0ca484595712
Ching Lam Choi <ccl5a09@gmail.com>
2020-02-11 04:46:12 +0800

    Update NNODE2_tests.jl

--- test/NNODE2_tests.jl
+++ test/NNODE2_tests.jl
@@ -1,25 +1,23 @@
 using Test, Flux
-using Plots
 using DiffEqDevTools
 using DiffEqBase, NeuralNetDiffEq
 
 # Homogeneous on scalar
-f(du, u, p, t) = -du+6*u
+f = (du, u, p, t) -> -du+6*u
 tspan = (0.0f0, 1.0f0)
 u0 = 1.0f0
 du0 = 0.0f0
 dt = 1/5f0
-function an_sol(du0, u0, p, t)
-  [1/5 * exp(-3*t) *(3*exp(5*t)+2.0)]
-end
-prob = SecondOrderODEProblem(ODEFunction{false}(f, analytic=an_sol), u0, du0, tspan)
+f_analytic = t -> [1/5 * exp(-3*t) *(3*exp(5*t)+2.0)]
+prob = SecondOrderODEProblem(ODEFunction{false}(f, analytic=f_analytic), u0, du0, tspan)
 chain = Chain(Dense(1,5,σ),Dense(5,1))
-opt = ADAM(1e-04, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true, maxiters = 200)
-#println(sol)
-err = sol.errors[:l2]
-sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=1/20f0, verbose = true, maxiters = 200)
-sol.errors[:l2]/err<0.5
+opt = ADAM(1, (0.9, 0.95))
+sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true, maxiters = 100)
+err = sqrt(sum((sol.u - [f_analytic(t)[1] for t in tspan[1]:dt:tspan[2]]).^2)[1])
+println("L2 Norm: ", err, "\n")
+sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=1/20f0, verbose = true, maxiters = 100)
+err = sqrt(sum((sol.u - [f_analytic(t)[1] for t in tspan[1]:1/20f0:tspan[2]]).^2)[1])
+println("L2 Norm: ", err, "\n")
 
 #=
 dts = 1f0 ./ 2f0 .^ (6:-1:2)
@@ -30,22 +28,21 @@ sim = test_convergence(dts, prob, NeuralNetDiffEq.NNODE2(chain, opt))
 
 
 #Example 2: Test on vector
-f(du, u, p, t) = -2*du-5*u+5*t^2+l2
+f = (du, u, p, t) -> -2*du-5*u+5*t^2+12
 tspan = (0.0f0, 1.0f0)
 u0 = [1.0f0]
 du0 = [0.0f0]
 dt = 1/5f0
-function an_sol(du0, u0, p, t)
-  [1/50 * exp(-t) * (2*exp(t) * (25*t^2 - 20*t + 58) - 13*sin(2*t) - 66*cos(2*t))]
-end
-prob = SecondOrderODEProblem(ODEFunction{false}(f, analytic=an_sol), u0, du0, tspan)
+f_analytic = t -> [1/50 * exp(-t) * (2*exp(t) * (25*t^2 - 20*t + 58) - 13*sin(2*t) - 66*cos(2*t))]
+prob = SecondOrderODEProblem(ODEFunction{false}(f, analytic=f_analytic), u0, du0, tspan)
 chain = Chain(Dense(1,5,σ),Dense(5,1))
 opt = ADAM(1e-04, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true, maxiters = 200)
-#println(sol)
-err = sol.errors[:l2]
-sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=1/20f0, verbose = true, maxiters = 200)
-sol.errors[:l2]/err < 0.5
+sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=dt, verbose = true, maxiters = 100)
+err = sqrt(sum((sol.u - [f_analytic(t)[1] for t in tspan[1]:dt:tspan[2]]).^2)[1])
+println("L2 Norm: ", err, "\n")
+sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=1/20f0, verbose = true, maxiters = 100)
+err = sqrt(sum((sol.u - [f_analytic(t)[1] for t in tspan[1]:1/20f0:tspan[2]]).^2)[1])
+println("L2 Norm: ", err, "\n")
 
 #=
 dts = 1f0 ./ 2f0 .^ (6:-1:2)
