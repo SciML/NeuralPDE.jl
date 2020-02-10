302c23415d8a6b8235d6e38a3c3321e4e2a0ad75
Kirill <kirill@Kirills-MacBook-Air.local>
2020-02-10 18:19:25 +0300

    APPVEYOR test fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,10 +11,10 @@ const is_TRAVIS = haskey(ENV,"TRAVIS")
   if GROUP == "All" || GROUP == "NNODE"
       @time @safetestset "NNODE" begin include("NNODE_tests.jl") end
   end
-  if GROUP == "All" || GROUP == "NNPDEHan"
+  if !is_APPVEYOR && (GROUP == "All" || GROUP == "NNPDEHan")
       @time @safetestset "NNPDEHan" begin include("NNPDEHan_tests.jl") end
   end
-  if !is_APPVEYOR && (GROUP == "All" || GROUP == "NNPDENS")
+  if GROUP == "All" || GROUP == "NNPDENS"
       @time @safetestset "NNPDENS" begin include("NNPDENS_tests.jl") end
   end
 end
