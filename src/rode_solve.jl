af5301939f4c6048658a478edc0d500f5db811fb
ashutosh-b-b <a.bharambe123@gmailcom>
2020-02-23 00:01:17 +0530

    Added Noise Grid to the test and changed solver

--- src/rode_solve.jl
+++ src/rode_solve.jl
@@ -1,8 +1,10 @@
-struct NNRODE{C,O} <: NeuralNetDiffEqAlgorithm
+struct NNRODE{C,Wg,O} <: NeuralNetDiffEqAlgorithm
     chain::C
+    Wg::Wg
     opt::O
+    
 end
-NNRODE(chain;opt=Flux.ADAM(0.1)) = NNRODE(chain,opt)
+NNRODE(chain , Wg ; opt=Flux.ADAM(0.1)) = NNRODE(chain, Wg , opt)
 
 function DiffEqBase.solve(
     prob::DiffEqBase.AbstractRODEProblem,
@@ -27,6 +29,7 @@ function DiffEqBase.solve(
     #hidden layer
     chain  = alg.chain
     opt    = alg.opt
+    Wg = alg.Wg
     ps     = Flux.params(chain)
     data   = Iterators.repeated((), maxiters)
 
@@ -37,15 +40,16 @@ function DiffEqBase.solve(
     ϕ(t,W) = u0 .- (t.-tspan[1]).*chain(Zygote.collect([t,W]))
     
     dfdx = (t,W) -> Zygote.gradient((t,W) -> sum(ϕ(t,W)), t,W)[1]
-    loss = () -> sum(abs2,sum(abs2,dfdx(t,W).-f(ϕ(t,W)[1],p,t,W)[1]) for (t,W) in zip(ts,randn(length(ts))))
-
+    loss = () -> sum(abs2,sum(abs2,dfdx(t,W).-f(ϕ(t,W)[1],p,t,W)[1]) for (t,W) in zip(ts, Wg.W))
     cb = function ()
         l = loss()
         verbose && println("Current loss is: $l")
         l < abstol && Flux.stop()
     end
     Flux.train!(loss, ps, data, opt; cb = cb)
-
-    u = [ϕ(t,W)[1].data for (t,W) in zip(ts,randn(length(ts)))]
+    u = Float64[]
+    for (t ,W) in zip(ts , Wg.u)
+        u = vcat(u , [ϕ(t,W)])
+    end
     u
 end #solve
--- test/NNRODE_tests.jl
+++ test/NNRODE_tests.jl
@@ -8,21 +8,30 @@ end
 u0 = 0.00
 tspan = (0.0,5.0)
 prob = RODEProblem(f,u0,tspan)
-
+dt = 0.1/20f0
+t = tspan[0]:dt:tspan[1]
+brownian_values = cumsum([0;[sqrt(dt1)*randn() for i in 1:length(t1)-2]])
+W = NoiseGrid(t1,brownian_values)
 chain = Flux.Chain(Dense(2,5,σ),Dense(5,16 ,σ ) , Dense(16,2))
 opt = Flux.ADAM(0.1, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNRODE(chain,opt), dt=1/20f0, verbose = true,
-            abstol=1e-10, maxiters = 1000)
+sol = solve(prob, NeuralNetDiffEq.NNRODE(chain,W,opt), dt=1/20f0, verbose = true,
+            abstol=1e-10, maxiters = 100)
+sol_real = sol_real = solve(prob,RandomEM(),dt=1/20f0)
 
 # Example 2
 f2 = (u,p,t,W) -> 1.01u.+0.87u.*W
 u0 = 1.00
 tspan = (0.0,1.0)
 prob = RODEProblem(f2,u0,tspan)
+dt = 0.1/20f0
+t = tspan[0]:dt:tspan[1]
+brownian_values = cumsum([0;[sqrt(dt1)*randn() for i in 1:length(t1)-2]])
+W = NoiseGrid(t1,brownian_values)
 chain = Flux.Chain(Dense(2,5,σ),Dense(5,16 ,σ ) , Dense(16,2))
 opt = Flux.ADAM(0.1, (0.9, 0.95))
-sol = solve(prob, NeuralNetDiffEq.NNRODE(chain,opt), dt=1/20f0, verbose = true,
-            abstol=1e-10, maxiters = 1000)
+sol = solve(prob, NeuralNetDiffEq.NNRODE(chain,W,opt), dt=1/20f0, verbose = true,
+            abstol=1e-10, maxiters = 100)
+sol_real = solve(prob,RandomEM(),dt=1/20f0)
 
 
 
