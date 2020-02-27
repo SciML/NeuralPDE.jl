81e5fa2784a8826bb6ef46d42f5897e05a91d79b
ashutosh-b-b <a.bharambe123@gmailcom>
2020-02-27 18:43:38 +0530

    Debug the error on the test

--- test/NNKolmogorov_tests.jl
+++ test/NNKolmogorov_tests.jl
@@ -37,7 +37,7 @@ end
 real_sol = analytical(sol[1])
 err = 0
 for (r , s) in zip(real_sol , sol[2] )
-    err  = ((r - s)^2 / s^2) + err
+    global err  = ((r - s)^2 / s^2) + err
 end
 errorl2 = err/size(sol[2])[2]
 println("error_l2 = ", errorl2, "\n")
