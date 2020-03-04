af4bca1704806685b545089410f9b877296804e2
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-03-03 23:35:51 -0500

    allow for other AD modes

--- src/ode_solve.jl
+++ src/ode_solve.jl
@@ -1,9 +1,10 @@
-struct NNODE{C,O} <: NeuralNetDiffEqAlgorithm
+struct NNODE{C,O,K} <: NeuralNetDiffEqAlgorithm
     chain::C
     opt::O
     autodiff::Bool
+    kwargs::K
 end
-NNODE(chain,opt=Optim.BFGS();autodiff=false) = NNODE(chain,opt,autodiff)
+NNODE(chain,opt=Optim.BFGS();autodiff=false,kwargs...) = NNODE(chain,opt,autodiff,kwargs)
 
 function DiffEqBase.solve(
     prob::DiffEqBase.AbstractODEProblem,
@@ -34,20 +35,20 @@ function DiffEqBase.solve(
     ts = tspan[1]:dt:tspan[2]
 
     if chain isa FastChain
-        θ = DiffEqFlux.initial_params(chain)
+        initθ = DiffEqFlux.initial_params(chain)
         #The phi trial solution
         if u0 isa Number
-            phi = (t,θ) -> u0 + (t - tspan[1])*first(chain([t],θ))
+            phi = (t,θ) -> u0 + (t-tspan[1])*first(chain([t],θ))
         else
-            phi = (t,θ) -> chain([t],θ)
+            phi = (t,θ) -> u0 + (t-tspan[1])*chain([t],θ)
         end
     else
-        θ,re  = Flux.destructure(chain)
+        initθ,re  = Flux.destructure(chain)
         #The phi trial solution
         if u0 isa Number
-            phi = (t,θ) -> u0 + (t - tspan[1])*first(re(θ)([t]))
+            phi = (t,θ) -> u0 + (t-tspan[1])*first(re(θ)([t]))
         else
-            phi = (t,θ) -> u0 + (t - tspan[1])*re(θ)([t])
+            phi = (t,θ) -> u0 + (t-tspan[1])*re(θ)([t])
         end
     end
 
@@ -57,20 +58,24 @@ function DiffEqBase.solve(
         dfdx = (t,θ) -> (phi(t+sqrt(eps(t)),θ) - phi(t,θ))/sqrt(eps(t))
     end
 
+    #=
+    square_square(x) = x.^2
+    function loss(θ)
+        _dfdx_diff = square_square.(dfdx.(ts,(θ,)) .- f.(phi.(ts,(θ,)),(p,),ts))
+        sum(sum(_dfdx_diff))
+    end
+    =#
+
     function inner_loss(t,θ)
         sum(abs2,dfdx(t,θ) - f(phi(t,θ),p,t))
     end
-
-    loss(θ) = sum(abs2,phi(tspan[1],θ) - u0) + sum(abs2,inner_loss(t,θ) for t in ts)
+    loss(θ) = sum(abs2,inner_loss(t,θ) for t in ts) # sum(abs2,phi(tspan[1],θ) - u0)
 
     cb = function (p,l)
         verbose && println("Current loss is: $l")
         l < abstol
     end
-    res = DiffEqFlux.sciml_train(loss, θ, opt; cb = cb, maxiters=maxiters)
-
-    @show phi(tspan[1],res.minimizer)
-    @show sum(abs2,phi(tspan[1],res.minimizer) - u0)
+    res = DiffEqFlux.sciml_train(loss, initθ, opt; cb = cb, maxiters=maxiters, alg.kwargs...)
 
     #solutions at timepoints
     if u0 isa Number
