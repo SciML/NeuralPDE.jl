c5aeefb45a3dae6e441f0134697eb8b9b422b084
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-12 12:59:56 +0800

    Update NNODE2_tests.jl

--- test/NNODE2_tests.jl
+++ test/NNODE2_tests.jl
@@ -34,3 +34,20 @@ err1 = sqrt(sum((sol.u - [f_analytic(t)[1] for t in tspan[1]:dt:tspan[2]]).^2)[1
 sol = solve(prob, NeuralNetDiffEq.NNODE2(chain,opt), dt=1/20f0, verbose = true, maxiters = 100)
 err2 = sqrt(sum((sol.u - [f_analytic(t)[1] for t in tspan[1]:1/20f0:tspan[2]]).^2)[1])
 @test err2/err1 < 0.5
+
+
+function f(ddu, du, u, p, t)
+	ddu[1] = (-p[3]*u[1]-p[4]*(u[1]-u[2]))/p[1]
+	ddu[2] = (-p[4]*(u[2]-u[1]))/p[2]
+end
+function f(du, u, p, t)
+	[(-p[3]*u[1]-p[4]*(u[1]-u[2]))/p[1], (-p[4]*(u[2]-u[1]))/p[2]]
+end
+p = [1.0f0, 1.0f0, 6.0f0, 4.0f0]
+u0 = [1.0f0, 0.0f0]
+du0 = [2.0f0, 0.0f0]
+prob = SecondOrderODEProblem{false}(f, [u0], [du0], (0.0f0, 3.0f0), p)
+dt = 1/20f0
+opt = ADAM(1e-03)
+chain = Chain(Dense(1,5,σ),Dense(5,2))
+sol = solve(prob, NeuralNetDiffEq.NNODE2(chain, opt), dt=dt, verbose=true, abstol=1e-10, maxiters=50)
