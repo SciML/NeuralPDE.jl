98c6fb83a7b05a47763b128ae43202c4e7349ae9
ashutosh-b-b <a.bharambe123@gmail.com>
2020-04-10 00:34:31 +0530

    Reduced the train error for the model

--- src/rode_solve.jl
+++ src/rode_solve.jl
@@ -3,9 +3,10 @@ struct NNRODE{C,W,O,P,K} <: NeuralNetDiffEqAlgorithm
     W::W
     opt::O
     initθ::P
+    autodiff::Bool
     kwargs::K
 end
-function NNRODE(chain,W,opt=Optim.BFGS(),init_params = nothing;kwargs...)
+function NNRODE(chain,W,opt=Optim.BFGS(),init_params = nothing;autodiff=false,kwargs...)
     if init_params === nothing
         if chain isa FastChain
             initθ = DiffEqFlux.initial_params(chain)
@@ -15,8 +16,9 @@ function NNRODE(chain,W,opt=Optim.BFGS(),init_params = nothing;kwargs...)
     else
         initθ = init_params
     end
-    NNRODE(chain,W,opt,initθ,kwargs)
+    NNRODE(chain,W,opt,initθ,autodiff,kwargs)
 end
+
 function DiffEqBase.solve(
     prob::DiffEqBase.AbstractRODEProblem,
     alg::NeuralNetDiffEqAlgorithm,
@@ -28,56 +30,67 @@ function DiffEqBase.solve(
     abstol = 1f-6,
     verbose = false,
     maxiters = 100)
+
     DiffEqBase.isinplace(prob) && error("Only out-of-place methods are allowed!")
+
     u0 = prob.u0
     tspan = prob.tspan
     f = prob.f
     p = prob.p
     t0 = tspan[1]
+
     #hidden layer
     chain  = alg.chain
     opt    = alg.opt
-    Wg = alg.W
+    autodiff = alg.autodiff
+    W = alg.W
     #train points generation
     ts = tspan[1]:dt:tspan[2]
     initθ = alg.initθ
+
     if chain isa FastChain
         #The phi trial solution
         if u0 isa Number
-            phi = (t,W,θ) -> u0 + (t-tspan[1])*first(chain(adapt(typeof(θ),collect([t,W])),θ))
+            phi = (t,W,θ) -> u0 + (t-tspan[1])*first(chain(adapt(typeof(θ),[t,W]),θ))
         else
-            phi = (t,W,θ) -> u0 + (t-tspan[1])*chain(adapt(typeof(θ),collect([t,W])),θ)
+            phi = (t,W,θ) -> u0 + (t-tspan[1])*chain(adapt(typeof(θ),[t,W]),θ)
         end
     else
         _,re  = Flux.destructure(chain)
         #The phi trial solution
         if u0 isa Number
-            phi = (t,W,θ) -> u0 + (t-tspan[1])*first(re(θ)(adapt(typeof(θ),collect([t,W]))))
+            phi = (t,W,θ) -> u0 + (t-t0)*first(re(θ)(adapt(typeof(θ),[t,W])))
         else
-            phi = (t,W,θ) -> u0 + (t-tspan[1])*re(θ)(adapt(typeof(θ),collect([t,W])))
+            phi = (t,W,θ) -> u0 + (t-t0)*re(θ)(adapt(typeof(θ),[t,W]))
         end
     end
-    # dfdx = (x,θ) -> ForwardDiff.gradient((x)->phi(x[1],x[2],θ),x)[1]
-    dfdx = (x,θ) -> (phi(x[1]+sqrt(eps(x[1])),x[2],θ) - phi(x[1],x[2],θ)) / sqrt(eps(x[1]))
+
+    if autodiff
+        # dfdx = (t,W,θ) -> ForwardDiff.derivative(t->phi(t,θ),t)
+    else
+        dfdx = (t,W,θ) -> (phi(t+sqrt(eps(t)),W,θ) - phi(t,W,θ))/sqrt(eps(t))
+    end
 
     function inner_loss(t,W,θ)
-        x = [t,W]
-        sum(abs2,dfdx(x,θ) - f(phi(t,W,θ),p,W,t))
+        sum(abs,dfdx(t,W,θ) - f(phi(t,W,θ),p,t,W))
     end
-    loss(θ) = sum(abs2,inner_loss(ts[i],Wg.W[i],θ) for i in 1:length(ts) )
+    loss(θ) = sum(abs2,inner_loss(ts[i],W.W[i],θ) for i in 1:length(ts)) # sum(abs2,phi(tspan[1],θ) - u0)
+
     cb = function (p,l)
         verbose && println("Current loss is: $l")
         l < abstol
     end
     res = DiffEqFlux.sciml_train(loss, initθ, opt; cb = cb, maxiters=maxiters, alg.kwargs...)
-    if u0 isa Number
-        # u = [first(phi(t,W.u,res.minimizer)) for (t , W) in zip(ts , Wg)]
-        u = [phi(ts[i],Wg.W[i],res.minimizer) for i in 1:length(ts)]
 
+    #solutions at timepoints
+
+    if u0 isa Number
+        u = [(phi(ts[i],W.W[i],res.minimizer)) for i in 1:length(ts)]
     else
-        u = [phi(ts[i],Wg.W[i],res.minimizer) for i in 1:length(ts)]
+        u = [(phi(ts[i],W.W[i],res.minimizer)) for i in 1:length(ts)]
     end
-    sol = DiffEqBase.build_solution(prob,alg,ts,u,calculate_error = false)
+
+    sol = DiffEqBase.build_solution(prob,alg,ts,u,W,calculate_error = false)
     DiffEqBase.has_analytic(prob.f) && DiffEqBase.calculate_solution_errors!(sol;timeseries_errors=true,dense_errors=false)
     sol
 end #solve
--- test/rode_tests.jl
+++ test/rode_tests.jl
@@ -1,21 +1,30 @@
 using  NeuralNetDiffEq, Flux , DifferentialEquations, Optim, Adapt
-f = (u,p,t,W) -> 1.01u.+0.87u.*W
-u0 = 1.00
-tspan = (0.0,1.0)
-dt = 0.1/50f0
-ts= tspan[1]:dt:tspan[2]
-W = NoiseGrid(collect(ts) , randn(length(ts)))
-prob = RODEProblem(f,u0,tspan , noise = W)
-
-chain = Flux.Chain(Dense(2,128,elu),Dense(128,128,sigmoid),Dense(128,1))
-opt = BFGS()
-sol = solve(prob, NeuralNetDiffEq.NNRODE(chain, W ,opt), dt=1/50f0,
-verbose = true, abstol=1e-10, maxiters = 100)
-
-sol_real = solve(prob,RandomEM(),dt=1/50)
-
 using Plots
-plot(sol_real,linewidth=5,title="Solution to the linear ODE with a thick line",
-     xaxis="Time (t)",yaxis="u(t) (in μm)",label="My Thick Line!")
-sol
-Flux.mse(sol.u , sol_real.u)
+# linear = (u,p,t,W) -> 10*cos(2*pi*t) + 5*W
+linear = (u,p,t,W) ->   2u*sin(W)
+tspan = (0.0f0, 5.0f0)
+u0 = 1.0f0
+ts = tspan[1]:1/20f0:tspan[2]
+t = ts
+dt = 1/20f0
+brownian_values = cumsum([0;[sqrt(dt)*randn() for i in 1:length(t)-1]])
+W = NoiseGrid(t,brownian_values)
+prob = RODEProblem(linear, u0 ,tspan, noise = W)
+chain = Flux.Chain(Dense(2,128,elu),Dense(128,1))
+opt = ADAM(0.001)
+sol = solve(prob, NeuralNetDiffEq.NNRODE(chain,W,opt), dt=1/20f0, verbose = true,
+            abstol=1e-10, maxiters = 2500)
+W2 = NoiseWrapper(W)
+prob1 = RODEProblem(linear , u0 , tspan , noise = W2)
+sol2 = solve(prob1,RandomEM(),dt=dt)
+# real_sol = solve(prob , RandomEM() , dt = 1/20)
+# plot(sol2,linewidth=5,title="Solution to the linear ODE with a thick line",
+#      xaxis="Time (t)",yaxis="u(t) (in μm)",label="My Thick Line!")
+#
+# plot!(sol,linewidth=5,title="Solution to the linear ODE with a thick line",
+#           xaxis="Time (t)",yaxis="u(t) (in μm)",label="My Sol!")
+#
+# Flux.mse(sol2.u , sol.u)
+
+
+# Flux.Optimiser(ExpDecay(1, 0.01, 9000, 1e-9), ADAM())
