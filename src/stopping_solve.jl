6fd4d58ebdd34a3d41e4ce482bbc609146ba9522
ashutosh-b-b <a.bharambe123@gmail.com>
2020-05-09 09:11:08 +0530

    Add Monte Carlo Approx and Optimise Un

--- src/stopping_solve.jl
+++ src/stopping_solve.jl
@@ -25,45 +25,71 @@ function DiffEqBase.solve(
     N = size(ts)
     T = tspan[2]
 
-    #hidden layer
     chain  = alg.chain
     opt    = alg.opt
     sdealg = alg.sdealg
-    ps     = Flux.params(chain)
-    ##Temporal Discretisation
-    sdeproblem = SDEProblem(μ,sigma,u0,tspan)
-    sol = solve(sdeproblem, sdealg ,dt=dt , save_everystep=true , kwargs...)
-    X = sol.u
 
-    ##Using the factorisation lemma
-    function Un(n , X)
-      if(n == 1)return m(X[1])[1]
-      else
-          return max(first(m(X[n])[n]) , n + 1 - N)*(1 - sum(Un(i , X) for i in 1:n-1))
-      end
-    end
+    prob = SDEProblem(μ,sigma,u0,tspan)
+    ensembleprob = EnsembleProblem(prob)
+    sim = solve(ensembleprob, sdealg(), EnsembleThreads(), dt=dt,trajectories=1000,adaptive=false)
 
-    function loss()
-        return sum(Un(i , X)*g(ts[i] , X[i]) for i in 1 : N)
-    end
+    payoff = []
+    times = []
+    iter = 0
+    sump = 0
+    for u in sim.u
+        X = u.u
+        m = chain
+        a = []
+        function Un(n , X )
+            if size(a)[1] >= n
+                return a[n]
+            else
+                if(n == 1)
+                      ans =  first(m([X[1]])[1])
+                      global a = [ans]
+                      return ans
+                  else
+                      ans =  max(first(m([X[n]])[n]) , n + 1 - N)*(1 - sum(Un(i , X ) for i in 1:n-1))
+                      global a = vcat( a , ans)
+                      return ans
+                  end
+              end
+        end
 
-    dataset = Iterators.repeated(() , maxiters)
-    epoch = 0
-    opt = ADAM(-0.1)
-    cb = function ()
-      l = loss()
-      epoch = epoch + 1
-      epoch%20 == 0 && println("Current loss is: $l")
+        function loss()
+          return 1000 - sum(Un(i , X )*g(ts[i] , X[i]) for i in 1 : N)
+        end
+        dataset = Iterators.repeated(() , 50)
+        epoch = 0
+        opt = ADAM(0.1)
+
+        cb = function ()
+            l = loss()
+            global a = []
+            global epoch = epoch + 1
+            epoch%10 == 0 && println("Current loss is: $l")
+        end
+        Flux.train!(loss, Flux.params(m), dataset, opt; cb = cb)
+
+        Usum = 0
+        ti = 0
+        for i in 1:N
+              a = []
+              Usum = Usum + Un(i , X)
+              if Usum >= 1 - Un(i , X)
+                ti = i
+                break
+              end
+        end
+        price = g(ts[ti] , X[ti])
+        global payoff = vcat(payoff , price)
+        global times = vcat(times, ti)
+        global iter = iter + 1
+        println(iter)
+        global sump = sump + price
+        # println("SUM : $sump")
+        # println("TIME : $ti")
     end
-    Flux.train!(loss, Flux.params(m), dataset, opt; cb = cb)
-    Usum = 0
-    time = 0
-    for i in 1:N
-        global Usum = Usum + Un(i , X)
-        if Usum >= 1 - Un(i , X)
-            time = i
-        break
-    end
-  end
-  g(ts[time] , X[time]) , time
+    sum(payoff)/size(payoff)[1] , sum(times)/size(times)[1]
  end #solve
