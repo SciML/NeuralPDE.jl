686541d95a5a049104dd711bc501d2e81338738c
Zing <zzj0402@gmail.com>
2020-06-26 17:07:08 +1200

    add to Tutorials

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
 using Documenter, NeuralNetDiffEq
 
 makedocs(
-    sitename = "NeuralNetDiffEq.jl",
+    sitename="NeuralNetDiffEq.jl",
     authors="#",
-    clean = true,
-    doctest = false,
-    modules = [NeuralNetDiffEq],
+    clean=true,
+    doctest=false,
+    modules=[NeuralNetDiffEq],
 
-    format = Documenter.HTML(#analytics = "",
-                             assets = ["assets/favicon.ico"],
+    format=Documenter.HTML(# analytics = "",
+                             assets=["assets/favicon.ico"],
                              canonical="#"),
     pages=[
         "Home" => "index.md",
         "Tutorials" => Any[
+            "100_HJB.md"
         ],
     ]
 )
 
 deploydocs(
-   repo = "github.com/SciML/NeuralNetDiffEq.jl.git";
-   push_preview = true
+   repo="github.com/SciML/NeuralNetDiffEq.jl.git";
+   push_preview=true
 )
--- install.jl
+++
@@ -1,5 +0,0 @@
-using Pkg
-Pkg.add("NeuralNetDiffEq")
-Pkg.add("Flux")
-Pkg.add("DifferentialEquations")
-Pkg.add("LinearAlgebra")
