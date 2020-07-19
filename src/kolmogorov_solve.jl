682d1f813d92dfceb6e72a14a7263bf7e016eeea
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-07-19 13:29:30 -0400

    Update kolmogorov_solve.jl

--- src/kolmogorov_solve.jl
+++ src/kolmogorov_solve.jl
@@ -9,10 +9,10 @@ Arguments:
 - `opt`: The optimiser to train the neural network. Defaults to `ADAM(0.1)`
 - `sdealg`: The algorithm used to solve the discretised SDE according to the process that X follows. Defaults to `EM()`
 - `ensemblealg`: The algorithm used to solve the Ensemble Problem that performs Ensemble simulations for the SDE. Defaults to `EnsembleThreads()`.See
-  the [Ensemble Algorithms](https://docs.sciml.ai/stable/features/ensemble/#EnsembleAlgorithms-1)
+  the [Ensemble Algorithms](https://diffeq.sciml.ai/stable/features/ensemble/#EnsembleAlgorithms-1)
   documentation for more details.
 - - `kwargs`: Additional arguments splatted to the SDE solver. See the
-  [Common Solver Arguments](https://docs.sciml.ai/dev/basics/common_solver_opts/)
+  [Common Solver Arguments](https://diffeq.sciml.ai/dev/basics/common_solver_opts/)
   documentation for more details.
 [1]Beck, Christian, et al. "Solving stochastic differential equations and Kolmogorov equations by means of deep learning." arXiv preprint arXiv:1806.00421 (2018).
 [2]
