48587efc308945bcdbccdde89888124d58b7b9e4
ashutosh-b-b <a.bharambe123@gmail.com>
2021-02-11 02:24:17 +0530

    Add domain changes, Fix time issue

--- src/param_kolmogorov_solve.jl
+++ src/param_kolmogorov_solve.jl
@@ -8,7 +8,7 @@ NNParamKolmogorov(chain  ; opt=Flux.ADAM(0.1) , sdealg = EM() , ensemblealg = En
 
 function DiffEqBase.solve(
     prob::ParamKolmogorovPDEProblem,
-    alg::NNKolmogorov;
+    alg::NNParamKolmogorov;
     abstol = 1f-6,
     verbose = false,
     maxiters = 300,
@@ -25,17 +25,18 @@ function DiffEqBase.solve(
     g = prob.g
     f = prob.f
     noise_rate_prototype = prob.noise_rate_prototype
-    yspan = prob.yspan
+    Y_domain = prob.Y_domain
     xspan = prob.xspan
     d = prob.d
     phi = prob.phi
     ts = tspan[1]:dt:tspan[2]
     xs = xspan[1]:dx:xspan[2]
-    γs = Y_domain[1]:dγ:Y_domain[2]
-
-    γ_mu_prototype = prob.γ_mu_prototype
-    γ_sigma_prototype = prob.γ_sigma_prototype
-    γ_phi_prototype = prob.γ_phi_prototype
+    γs_sigma = Y_domain.sigma[1]:dγ:Y_domain.sigma[2]
+    γs_mu = Y_domain.mu[1]:dγ:Y_domain.mu[2]
+    γs_phi = Y_domain.phi[1]:dγ:Y_domain.phi[2]
+    γ_mu_prototype = prob.Y_mu_prototype
+    γ_sigma_prototype = prob.Y_sigma_prototype
+    γ_phi_prototype = prob.Y_phi_prototype
 
     #hidden layer
     chain  = alg.chain
@@ -43,10 +44,6 @@ function DiffEqBase.solve(
     sdealg = alg.sdealg
     ensemblealg = alg.ensemblealg
     ps     = Flux.params(chain)
-    γ_sigma = rand(γs , d , d , d + 1, trajectories)
-    γ_phi = rand(γs , k , 1 , 1, trajectories)
-    γ_mu_1 = rand(γs , d , d , 1, trajectories)
-    γ_mu_2 = rand(γs , d , 1 , 1, trajectories)
     t = rand(ts , 1 , trajectories)
     x = rand(xs , d , 1 , trajectories)
 
@@ -55,7 +52,7 @@ function DiffEqBase.solve(
     if isnothing(γ_sigma_prototype)
       γ_sigma = nothing
     else
-      γ_sigma = rand(γs , size(γ_sigma_prototype)[1] , size(γ_sigma_prototype)[2] , size(γ_sigma_prototype)[3] , trajectories)
+      γ_sigma = rand(γs_sigma , size(γ_sigma_prototype)[1] , size(γ_sigma_prototype)[2] , size(γ_sigma_prototype)[3] , trajectories)
       total_dims += d^2*(size(γ_sigma_prototype))[3]
     end
     if isnothing(γ_mu_prototype)
@@ -63,22 +60,22 @@ function DiffEqBase.solve(
       γ_mu_2 = nothing
     else
       if !isnothing(γ_mu_prototype[1])
-        γ_mu_1 = rand(γs , size(γ_mu_prototype[1])[1], size(γ_mu_prototype[1])[2], 1 , trajectories)
+        γ_mu_1 = rand(γs_mu , size(γ_mu_prototype[1])[1], size(γ_mu_prototype[1])[2], 1 , trajectories)
         total_dims += d^2
       elseif !isnothing(γ_mu_prototype[1])
-        γ_mu_2 = rand(γs , size(γ_mu_prototype[2])[1], size(γ_mu_prototype[2])[2] , 1 ,trajectories)
+        γ_mu_2 = rand(γs_mu , size(γ_mu_prototype[2])[1], size(γ_mu_prototype[2])[2] , 1 ,trajectories)
         total_dims += d
       end
     end
     if isnothing(γ_phi_prototype)
       γ_phi = nothing
     else
-      γ_phi = rand(γs , size(γ_phi_prototype)[1] , size(γ_phi_prototype)[2] , 1 , trajectories)
+      γ_phi = rand(γs_phi , size(γ_phi_prototype)[1] , 1 , trajectories)
       total_dims += size(γ_phi_prototype)[1]
     end
 
     #Declare wrapper functions to mu and sigma
-    function sigma_(x , γ_sigma , i)
+    function sigma_(dx , x , γ_sigma , i)
       if isnothing(γ_sigma)
         return g(x , γ_sigma)
       else
@@ -86,7 +83,7 @@ function DiffEqBase.solve(
       end
     end
 
-    function mu_(x , γ_mu_1, γ_mu_2 , i)
+    function mu_(dx, x , γ_mu_1, γ_mu_2 , i)
       if isnothing(γ_mu_prototype)
         return f(x , γ_mu_1 , γ_mu_2)
       else
@@ -102,9 +99,8 @@ function DiffEqBase.solve(
       end
     end
 
-
     #Preparing training data
-    X  = reshape([], total_dims ,0)
+    X  = zeros(total_dims ,trajectories)
     for i in 1:length(t)
       K = vcat(t[i] , x[: , : , i])
       if !isnothing(γ_sigma_prototype)
@@ -114,23 +110,23 @@ function DiffEqBase.solve(
         K = vcat(K , reshape(γ_mu_1[: , : , : , i] , d^2 , 1) , reshape(γ_mu_2[: , : , : , i]  , d , 1))
       end
       if !isnothing(γ_phi_prototype)
-        K = vcat(K , reshape(γ_mu_1[: , : , : , i] , d^2 , 1) , reshape(γ_mu_2[: , : , : , i]  , d , 1))
+        K = vcat(K , reshape(γ_phi[: , :  , i] , size(γ_phi_prototype)[1] , 1))
       end
-      X = hcat(X , K)
+      X[: , i] = K
     end
     print("Data Prepared to train the model")
 
-    function sigma(x , p , t)
-      sigma_(x , γ_sigma , 1)
+    function sigma(dx , x , p , t)
+      sigma_(dx , x , γ_sigma , 1)
     end
-    function mu(x , p , t)
-      mu_(x , γ_mu_1, γ_mu_2 , 1)
+    function mu(dx, x , p , t)
+      mu_(dx, x , γ_mu_1, γ_mu_2 , 1)
     end
 
     sdeproblem = SDEProblem(mu,sigma,x[: , : , 1], (0.00 , t[1]))
     function prob_func(prob,i,repeat)
-      sigma(x , p , t) = sigma_(x , γ_sigma , i)
-      mu(x , p , t) = mu_(x , γ_mu_1, γ_mu_2 , i)
+      sigma(dx , x , p , t) = sigma_(dx , x , γ_sigma , i)
+      mu(dx , x , p , t) = mu_(dx , x , γ_mu_1, γ_mu_2 , i)
       SDEProblem(prob.f , prob.g , x[: , : , i] , (0.00 , t[i]) ,noise_rate_prototype = prob.noise_rate_prototype)
     end
     output_func(sol,i) = (sol[end],false)
@@ -139,18 +135,27 @@ function DiffEqBase.solve(
     x_sde  = reshape([],d,0)
     # sol = solve(sdeproblem, sdealg ,dt=0.01 , save_everystep=false , kwargs...)
     # x_sde = sol[end]
-    for u in sim.u
-        x_sde = hcat(x_sde , u)
+
+    x_sde  = zeros(1,trajectories)
+    for i in 1:length(sim.u)
+        x_sde[: , i] = sim.u[i]
     end
+
     y = phi(x_sde , γ_phi)
-    y = phi(x_sde)
+
+    #Check the dimensions for the target
+    if size(y)[1] != 1 && size(y)[1] != trajectories
+      errMsg = "The output from phi function has dimensions "*string(size(y))*" while it should be (1, trajectories)"
+      throw(DimensionMismatch(errMsg))
+    end
+
     if use_gpu == true
-        y = y |>gpu
+        y = y |> gpu
         X = X |> gpu
     end
-    data   = Iterators.repeated((X , y), 1000)
+    data   = Iterators.repeated((X , y), maxiters)
     if use_gpu == true
-        data = data |>gpu
+        data = data |> gpu
     end
 
     #MSE Loss Function
@@ -163,6 +168,5 @@ function DiffEqBase.solve(
     end
 
     Flux.train!(loss, ps, data, opt; cb = cb)
-
 
  end #solve
