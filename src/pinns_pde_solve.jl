a58ff6cba22103c0c44f48fdbde407b09ec6c156
KirillZubov <kirillzubov3@gmail.com>
2021-02-19 19:31:17 +0300

    fix

--- src/pinns_pde_solve.jl
+++ src/pinns_pde_solve.jl
@@ -718,8 +718,8 @@ function DiffEqBase.discretize(pde_system::PDESystem, discretization::PhysicsInf
         pde_train_set, bcs_train_sets = train_sets
         pde_train_set = [pde_train_set]
 
-        pde_train_set = adapt(typeof(initθ),pde_train_set)
-        bcs_train_sets =  adapt.(typeof(initθ),pde_train_set)
+        pde_train_set = adapt.(typeof(initθ),pde_train_set)
+        bcs_train_sets =  adapt.(typeof(initθ),bcs_train_sets)
 
         pde_loss_function = get_loss_function(_pde_loss_functions,
                                                         pde_train_set, #TODO general
