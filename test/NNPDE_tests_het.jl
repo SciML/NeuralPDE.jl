24dd516b48291449b20d5dc12302550e52a91de0
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-08-18 09:10:42 +0200

    changed chain in test

--- test/NNPDE_tests_het.jl
+++ test/NNPDE_tests_het.jl
@@ -7,8 +7,7 @@ Dx = Differential(x)
 Dy = Differential(y)
 
  # 2D PDE
-eq  = p(x) + q(y) ~ 0
- # eq  = Dx(p(x)) + Dy(q(y)) + Dx(r(x, y)) + Dy(s(y, x)) + p(x) + q(y) + r(x, y) + s(y, x) ~ 0
+eq  = Dx(p(x)) + Dy(q(y)) + Dx(r(x, y)) + Dy(s(y, x)) + p(x) + q(y) + r(x, y) + s(y, x) ~ 0
 
  # Initial and boundary conditions
 bcs = [p(1) ~ 0.f0, q(-1) ~ 0.0f0,
@@ -21,9 +20,10 @@ domains = [x ∈ IntervalDomain(0.0, 1.0),
 
  # chain_ = FastChain(FastDense(2,12,Flux.σ),FastDense(12,12,Flux.σ),FastDense(12,1))
 numhid = 3
-fastchains = FastChain(FastDense(1, numhid, Flux.σ), FastDense(numhid, numhid, Flux.σ), FastDense(numhid, 1))
+fastchains = [[FastChain(FastDense(1, numhid, Flux.σ), FastDense(numhid, numhid, Flux.σ), FastDense(numhid, 1)) for i in 1:2];
+               [FastChain(FastDense(2, numhid, Flux.σ), FastDense(numhid, numhid, Flux.σ), FastDense(numhid, 1)) for i in 1:2]]
 discretization = NeuralPDE.PhysicsInformedNN(fastchains,
-                                                 GridTraining(0.5))
+                                                 StochasticTraining(30))
 
 _indvars = [x,y]
 _depvars = [p(x), q(y), r(x, y), s(y, x)]
@@ -39,60 +39,6 @@ initθvec = vcat(initθ...)
 prob.f(initθvec, [])
 res = GalacticOptim.solve(prob, ADAM(0.1); maxiters=3)
 phi = discretization.phi
-eqs = pde_system.eqs
-bcs = pde_system.bcs
-
-domains = pde_system.domain
-eq_params = pde_system.ps
-defaults = pde_system.defaults
-default_p = eq_params == SciMLBase.NullParameters() ? nothing : [defaults[ep] for ep in eq_params]
- param_estim = discretization.param_estim
- additional_loss = discretization.additional_loss
-
- # dimensionality of equation
- dim = length(domains)
- depvars, indvars, dict_indvars, dict_depvars, dict_depvar_input = NeuralPDE.get_vars(pde_system.indvars, pde_system.depvars)
-
- chain = discretization.chain
- initθ = discretization.init_params
- flat_initθ = if (typeof(chain) <: AbstractVector) vcat(initθ...) else  initθ end
- flat_initθ = if param_estim == false flat_initθ else vcat(flat_initθ, adapt(DiffEqBase.parameterless_type(flat_initθ), default_p)) end
- phi = discretization.phi
-derivative = discretization.derivative
- strategy = discretization.strategy
- if !(eqs isa Array)
-     eqs = [eqs]
- end
- pde_indvars = if strategy isa QuadratureTraining
-         NeuralPDE.get_argument(eqs, dict_indvars, dict_depvars)
- else
-         NeuralPDE.get_variables(eqs, dict_indvars, dict_depvars)
- end
- _pde_loss_functions = [NeuralPDE.build_loss_function(eq,indvars,depvars,
-                                             dict_indvars,dict_depvars,dict_depvar_input,
-                                             phi, derivative,chain, initθ,strategy,eq_params=eq_params,param_estim=param_estim,default_p=default_p,
-                                             bc_indvars=pde_indvar) for (eq, pde_indvar) in zip(eqs, pde_indvars)]
- bc_indvars = if strategy isa QuadratureTraining
-         get_argument(bcs, dict_indvars, dict_depvars)
- else
-         get_variables(bcs, dict_indvars, dict_depvars)
- end
-
- _bc_loss_functions = [build_loss_function(bc,indvars,depvars,
-                                                 dict_indvars,dict_depvars,dict_depvar_input,
-                                                 phi, derivative,chain, initθ, strategy,eq_params=eq_params,param_estim=param_estim,default_p=default_p;
-                                                 bc_indvars=bc_indvar) for (bc, bc_indvar) in zip(bcs, bc_indvars)] 
-
-
- prob.f(initθvec, [])
-
-
-
-
-
-
-
-
 
 
 
