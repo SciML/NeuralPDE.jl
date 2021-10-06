46ed59ee4953c753f3f987fec5ae06d7c9fdb350
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-10-06 17:51:19 +0200

    just pushing to run the CI again. Sorry!

--- src/pinns_pde_solve.jl
+++ src/pinns_pde_solve.jl
@@ -304,7 +304,7 @@ function _transform_expression(ex,indvars,depvars,dict_indvars,dict_depvars,dict
                                         end
                                       end
                                 
-                num_depvar = map(int_depvar -> dict_depvars[int_depvar], integrating_depvars)
+                num_depvar = map(int_depvar -> dict_depvars[int_depvar], integrating_depvars) 
                 integrand = transform_expression(_args[2],indvars,depvars,dict_indvars,dict_depvars, dict_depvar_input, chain,eltypeθ,strategy,phi,derivative_,integral,initθ; is_integral = true)
                 integrand = build_symbolic_loss_function(nothing, indvars,depvars,dict_indvars,dict_depvars, dict_depvar_input, phi, derivative_, nothing, chain, initθ, strategy, integrand = integrand, integrating_depvars=integrating_depvars, eq_params=SciMLBase.NullParameters(), param_estim =false, default_p = nothing)
                 # integrand = repr(integrand)
