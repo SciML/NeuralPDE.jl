a5d42fcc9cf8c2d08b59aca91c0f57f839d7dd89
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-12-09 07:25:51 -0800

    changed to a different system

--- test/IDE_tests.jl
+++ test/IDE_tests.jl
@@ -205,30 +205,30 @@ w_real  = [1/x^2 for x in xs]
 # plot(xs,w_real)
 # plot!(xs,w_predict)
 
-println("Improper (a, Inf) Integral Test")
+println("Improper (0, Inf) Integral Test")
 
 @parameters x
 @variables u(..)
-I = Integral(x in ClosedInterval(1, Inf))
-eqs_ = I(u(x)) ~ 1
+I = Integral(x in ClosedInterval(0, Inf))
+eqs = I(u(x)) ~ π/(3*sqrt(3))
 
 domains = [x ∈ Interval(1.0, 2.0)]
 
-bcs = [u(1) ~ 1, u(2) ~ 1/4]
+bcs = [u(1.) ~ exp(1)/(exp(2) + 3)]
 
 # Neural Network
 chain = FastChain(FastDense(1, 16, Flux.σ), FastDense(16,16,Flux.σ), FastDense(16, 1))
 initθ = map(c -> Float64.(c), DiffEqFlux.initial_params.(chain))
 
-discretization = NeuralPDE.PhysicsInformedNN(chain, GridTraining(0.05), init_params= initθ)
-@named pde_system = PDESystem(eqs_, bcs, domains, [x], [u(x)])
+discretization = NeuralPDE.PhysicsInformedNN(chain, GridTraining(0.1), init_params= initθ)
+@named pde_system = PDESystem(eqs, bcs, domains, [x], [u(x)])
 prob = SciMLBase.symbolic_discretize(pde_system, discretization)
 prob = SciMLBase.discretize(pde_system, discretization)
 res = GalacticOptim.solve(prob, BFGS(); cb=cb, maxiters=100)
 xs = [infimum(d.domain):0.01:supremum(d.domain) for d in domains][1]
 phi = discretization.phi
 u_predict  = [first(phi([x],res.minimizer)) for x in xs]
-u_real  = [1/x^2 for x in xs]
+u_real  = [exp(x)/(exp(2*x) + 3) for x in xs]
 @test Flux.mse(u_real, u_predict) < 0.01
 
 
@@ -256,4 +256,4 @@ xs = [infimum(d.domain):0.01:supremum(d.domain) for d in domains][1]
 phi = discretization.phi
 u_predict  = [first(phi([x],res.minimizer)) for x in xs]
 u_real  = [x*exp(-x^2) for x in xs]
-@test Flux.mse(u_real, u_predict) < 0.01
+@test Flux.mse(u_real, u_predict) < 0.015
