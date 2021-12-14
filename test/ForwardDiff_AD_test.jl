da8a113bd9cf44620f814222ccbee817d1221777
KirillZubov <kirillzubov3@gmail.com>
2021-12-14 17:33:58 +0300

    PDE systems and Heterogeneous input

--- test/ForwardDiff_AD_test.jl
+++ test/ForwardDiff_AD_test.jl
@@ -217,3 +217,151 @@ diff_u = [[abs(u_analytic(x,t) -first(phi([x,t],res.minimizer)))  for x in xs] f
 # p2 =plot(xs,u_real,title = "analytic")
 # p3 =plot(xs,diff_u,title = "error")
 # plot(p1,p2,p3)
+
+
+println(" system of pde")
+@parameters x, y
+@variables u1(..), u2(..)
+Dx = Differential(x)
+Dy = Differential(y)
+eqs = [Dx(u1(x,y)) + 4*Dy(u2(x,y)) ~ 0,
+      Dx(u2(x,y)) + 9*Dy(u1(x,y)) ~ 0]
+      # 3*u1(x,0) ~ 2*u2(x,0)]
+bcs = [u1(x,0) ~ 2*x, u2(x,0) ~ 3*x]
+domains = [x ∈ Interval(0.0,1.0), y ∈ Interval(0.0,1.0)]
+chain1 = FastChain(FastDense(2,5,Flux.tanh),FastDense(5,1))
+chain2 = FastChain(FastDense(2,5,Flux.tanh),FastDense(5,1))
+
+# quadrature_strategy = NeuralPDE.QuadratureTraining(quadrature_alg=CubatureJLh(),
+#                                                     reltol=1e-3,abstol=1e-3,
+#                                                     maxiters =50, batch=100)
+
+strategy = NeuralPDE.GridTraining(0.1)
+chain = [chain1,chain2]
+initθ = map(c -> Float64.(c), DiffEqFlux.initial_params.(chain))
+
+discretization = NeuralPDE.PhysicsInformedNN(chain,strategy; init_params = initθ, AD=true)
+
+@named pde_system = PDESystem(eqs,bcs,domains,[x,y],[u1(x, y),u2(x, y)])
+
+prob = NeuralPDE.discretize(pde_system,discretization)
+sym_prob = NeuralPDE.symbolic_discretize(pde_system,discretization)
+
+flat_initθ = reduce(vcat,initθ)
+prob.f(flat_initθ, nothing)
+ForwardDiff.gradient(θ -> prob.f(θ, nothing),flat_initθ)
+
+res = GalacticOptim.solve(prob,BFGS();cb=cb, maxiters=1000)
+prob = remake(prob,u0=res.minimizer)
+res = GalacticOptim.solve(prob,BFGS();cb=cb, maxiters=1000)
+phi = discretization.phi
+
+analytic_sol_func(x,y) =[1/3*(6x - y), 1/2*(6x - y)]
+xs,ys = [infimum(d.domain):0.01:supremum(d.domain) for d in domains]
+u_real  = [[analytic_sol_func(x,y)[i] for x in xs  for y in ys] for i in 1:2]
+
+initθ = discretization.init_params
+acum =  [0;accumulate(+, length.(initθ))]
+sep = [acum[i]+1 : acum[i+1] for i in 1:length(acum)-1]
+minimizers = [res.minimizer[s] for s in sep]
+u_predict  = [[phi[i]([x,y],minimizers[i])[1] for x in xs  for y in ys] for i in 1:2]
+
+error_  = u_predict .- u_real
+@test u_predict[1] ≈ u_real[1] rtol = 10^-4
+@test u_predict[2] ≈ u_real[2] rtol = 10^-4
+
+# p1 =plot(xs, ys, u_predict, st=:surface);
+# p2 = plot(xs, ys, u_real, st=:surface);
+# p3 = plot(xs, ys, error_[2],st=:surface);
+# plot(p1,p2,p3)
+
+println("Heterogeneous system")
+
+@parameters x,y,z
+@variables u(..), v(..), h(..), p(..)
+Dz = Differential(z)
+eqs = [
+    u(x,y,z) ~ x+y+z,
+    v(y,x) ~ x^2 + y^2,
+    h(z) ~ cos(z),
+    p(x,z) ~ exp(x)*exp(z),
+    u(x,y,z) + v(y,x)*Dz(h(z)) - p(x,z) ~ x+y+z - (x^2+y^2)*sin(z) - exp(x)*exp(z)
+]
+
+bcs = [u(0,0,0) ~ 0.0]
+
+domains = [x ∈ Interval(0.0, 1.0),
+           y ∈ Interval(0.0, 1.0),
+           z ∈ Interval(0.0, 1.0)]
+
+chain = [FastChain(FastDense(3,9,Flux.tanh),FastDense(9,1)),
+         FastChain(FastDense(2,9,Flux.tanh),FastDense(9,1)),
+         FastChain(FastDense(1,9,Flux.tanh),FastDense(9,1)),
+         FastChain(FastDense(2,9,Flux.tanh),FastDense(9,1))]
+
+initθ = map(c -> Float64.(c), DiffEqFlux.initial_params.(chain))
+
+
+grid_strategy = NeuralPDE.GridTraining(0.1)
+discretization = NeuralPDE.PhysicsInformedNN(chain,grid_strategy;init_params = initθ,AD=true)
+
+@named pde_system = PDESystem(eqs,bcs,domains,[x,y,z],[u(x,y,z),v(y,x),h(z),p(x,z)])
+
+prob = NeuralPDE.discretize(pde_system,discretization)
+sym_prob = NeuralPDE.symbolic_discretize(pde_system,discretization)
+sym_prob[1][end]
+  #   u(x,y,z) + v(y,x)*Dz(h(z)) - p(x,z) ~ x+y+z - (x^2+y^2)*sin(z) - exp(x)*exp(z)
+  # (+).((+).((*).(-1, u(cord4, var"##θ#4214", phi4)),
+  # (*).(derivative(Main.NeuralPDE.var"#694#697"(), var"##θ#4213", z),
+  # u(cord2, var"##θ#4212", phi2))), u(cord1, var"##θ#4211", phi1))
+  # .- (+).((+).((+).((+).(x, y), z), (*).((*).(-1, (exp).(x)), (exp).(z))),
+  # (*).((*).(-1, (+).((^).(x, 2), (^).(y, 2))), (sin).(z)))
+
+flat_initθ = reduce(vcat,initθ)
+prob.f(flat_initθ, nothing)
+# ForwardDiff.gradient(θ -> prob.f(θ, nothing),flat_initθ)
+
+res = GalacticOptim.solve(prob, BFGS();cb=cb,maxiters=1000)
+phi = discretization.phi
+
+analytic_sol_func_ =
+[
+(x,y,z) -> x+y+z ,
+(x,y) -> x^2 + y^2,
+(z) -> cos(z),
+(x,z) -> exp(x)*exp(z)
+]
+
+xs,ys,zs = [infimum(d.domain):0.1:supremum(d.domain) for d in domains]
+
+u_real = [analytic_sol_func_[1](x,y,z) for x in xs  for y in ys for z in zs]
+v_real = [analytic_sol_func_[2](y,x) for y in ys for x in xs ]
+h_real = [analytic_sol_func_[3](z) for z in zs]
+p_real = [analytic_sol_func_[4](x,z) for x in xs for z in zs]
+
+real_ = [u_real,v_real,h_real,p_real]
+
+initθ = discretization.init_params
+acum =  [0;accumulate(+, length.(initθ))]
+sep = [acum[i]+1 : acum[i+1] for i in 1:length(acum)-1]
+minimizers = [res.minimizer[s] for s in sep]
+
+
+u_predict = [phi[1]([x,y,z],minimizers[1])[1] for x in xs  for y in ys for z in zs]
+v_predict = [phi[2]([y,x],minimizers[2])[1] for y in ys for x in xs ]
+h_predict = [phi[3]([z],minimizers[3])[1] for z in zs]
+p_predict = [phi[4]([x,z],minimizers[4])[1] for x in xs for z in zs]
+predict = [u_predict,v_predict,h_predict,p_predict]
+
+for i in 1:4
+    @test predict[i] ≈ real_[i] rtol = 10^-2
+end
+#
+# x_plot = collect(xs)
+# y_plot = collect(ys)
+# i=1
+# z=0
+# u_real = collect(analytic_sol_func_[1](x,y,z) for y in ys, x in xs);
+# u_predict = collect(phi[1]([x,y,z],minimizers[1])[1]  for y in ys, x in xs);
+# plot(x_plot,y_plot,u_real)
+# plot!(x_plot,y_plot,u_predict)
