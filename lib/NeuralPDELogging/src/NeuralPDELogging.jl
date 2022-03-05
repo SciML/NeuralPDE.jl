7c485fa9876e506ca2c2fe6cbc859ce845d78b67
Zoe McCarthy <zoemccarthy12@gmail.com>
2022-03-04 20:20:32 -0500

    just the subpackage as a commit

---
+++ lib/NeuralPDELogging/src/NeuralPDELogging.jl
@@ -0,0 +1,26 @@
+module NeuralPDELogging
+
+using NeuralPDE
+
+"""This function overrides the function in NeuralPDE in order to use TensorBoardLogger in that package"""
+function logvector(logger, v, name, step)
+    if logger isa TBLogger
+        for j in 1:length(v)
+            log_value(logger, "$(name)/$(j)", v[j], step=step)
+        end
+    end
+end
+
+"""This function overrides the function in NeuralPDE in order to use TensorBoardLogger in that package"""
+function logscalar(logger, s, name, step)
+    if logger isa TBLogger
+        log_value(logger, "$(name)", s, step=step)
+    end
+end
+
+NeuralPDE.logvector = logvector
+NeuralPDE.logscalar = logscalar
+
+export logvector, logscalar
+
+end
