5d3ee90eab4216f0ce6c41a31749d02d96e3b36e
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-02 08:46:59 -0400

    move out pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,7 @@
 using Documenter, NeuralPDE
 
+include("pages.jl")
+
 makedocs(
     sitename="NeuralPDE.jl",
     authors="#",
@@ -10,42 +12,7 @@ makedocs(
     format=Documenter.HTML(  analytics = "UA-90474609-3",
                              assets=["assets/favicon.ico"],
                              canonical="https://neuralpde.sciml.ai/stable/"),
-    pages=[
-        "NeuralPDE.jl: Scientific Machine Learning (SciML) for Partial Differential Equations" => "index.md",
-        "Physics-Informed Neural Network Tutorials" => Any[
-            "pinn/poisson.md",
-            "pinn/wave.md",
-            "pinn/2D.md",
-            "pinn/system.md",
-            "pinn/3rd.md",
-            "pinn/low_level.md",
-            "pinn/ks.md",
-            "pinn/fp.md",
-            "pinn/parm_estim.md",
-            "pinn/heterogeneous.md",
-            "pinn/integro_diff.md",
-            "pinn/debugging.md",
-            "pinn/neural_adapter.md",
-        ],
-        "Specialized Neural PDE Tutorials" => Any[
-            "examples/100_HJB.md",
-            "examples/blackscholes.md",
-            "examples/kolmogorovbackwards.md",
-            "examples/optimal_stopping_american.md",
-        ],
-        "Specialized Neural ODE Tutorials" => Any[
-            "examples/ode.md",
-            "examples/nnrode_example.md",
-        ],
-        "API Documentation" => Any[
-            "solvers/pinns.md",
-            "solvers/deep_fbsde.md",
-            "solvers/kolmogorovbackwards_solver.md",
-            "solvers/optimal_stopping.md",#TODO
-            "solvers/ode.md",
-            "solvers/nnrode.md",#TODO
-        ]
-    ]
+    pages=pages
 )
 
 deploydocs(
