6d1f03ff9eb83a6ef8cff7482cfc0895ec178074
Yicheng Wu <yicheng.wu@ucalgary.ca>
2022-09-24 10:06:37 -0600

    Update src/symbolic_utilities.jl
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- src/symbolic_utilities.jl
+++ src/symbolic_utilities.jl
@@ -80,7 +80,7 @@ function transform_expression(pinnrep::PINNRepresentation, ex; is_integral = fal
     return ex
 end
 
-function get_ε(dim, der_num, eltypeθ, order)
+function get_ε(dim, der_num, ::Type{eltypeθ}, order) where eltypeθ
     epsilon = ^(eps(eltypeθ), one(eltypeθ) / (2 + order))
     ε = zeros(eltypeθ, dim)
     ε[der_num] = epsilon
