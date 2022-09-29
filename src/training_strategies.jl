a4f1ff2828d396149a5891cade71bab5fad77828
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-09-29 16:10:45 -0600

    Update training_strategies.jl

--- src/training_strategies.jl
+++ src/training_strategies.jl
@@ -194,7 +194,7 @@ function get_loss_function(loss_function, bound, eltypeθ, strategy::QuasiRandom
     loss = if resampling == true
         θ -> begin
             sets = QuasiMonteCarlo.sample(points, bound[1], bound[2], sampling_alg)
-            sets_ = adapt(parameterless_type(ComponentArrays.getdata(θ)), sets)
+            sets_ = ChainRulesCore.@ignore_derivatives adapt(parameterless_type(ComponentArrays.getdata(θ)), sets)
             mean(abs2, loss_function(sets_, θ))
         end
     else
