a15f6eb333affeb0b86e68ed37361217a1fffc96
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2023-04-02 02:46:18 +0530

    Added Tests for additional_loss function feature Also updated docs(might need further editing) Future Scope: We can add weighted loss in NNODE,and even losses for Parameter Estimation for Inverse Problems.

--- test/NNODE_tests.jl
+++ test/NNODE_tests.jl
@@ -228,3 +228,97 @@ alg = NeuralPDE.NNODE(chain, opt, autodiff = false,
 sol = solve(prob_oop, alg, verbose = true, maxiters = 100000, saveat = 0.01)
 
 @test abs(mean(sol) - mean(true_sol)) < 0.2
+
+# Checking if additional_loss feature works for NNODE
+linear = (u, p, t) -> cos(2pi * t)
+linear_analytic = (u, p, t) -> (1 / (2pi)) * sin(2pi * t)
+tspan = (0.0f0, 1.0f0)
+dt = (tspan[2] - tspan[1]) / 99
+ts = collect(tspan[1]:dt:tspan[2])
+prob = ODEProblem(ODEFunction(linear, analytic = linear_analytic), 0.0f0, (0.0f0, 1.0f0))
+opt = OptimizationOptimisers.Adam(0.1, (0.9, 0.95))
+
+# Analytical solution
+u_analytical(x) = (1 / (2pi)) .* sin.(2pi .* x)
+
+# GridTraining (Flux Chain)
+chain = Flux.Chain(Dense(1, 5, σ), Dense(5, 1))
+
+(u_, t_) = (u_analytical(ts), ts)
+function additional_loss(phi, θ)
+    return sum(sum(abs2, [phi(t, θ) for t in t_] .- u_)) / length(u_)
+end
+
+alg1 = NeuralPDE.NNODE(chain, opt, strategy = GridTraining(0.01),
+                       additional_loss = additional_loss)
+
+sol1 = solve(prob, alg1, verbose = true, abstol = 1.0f-8, maxiters = 500)
+@test sol1.errors[:l2] < 0.5
+
+# GridTraining (Lux Chain)
+luxchain = Lux.Chain(Lux.Dense(1, 5, Lux.σ), Lux.Dense(5, 1))
+
+(u_, t_) = (u_analytical(ts), ts)
+function additional_loss(phi, θ)
+    return sum(sum(abs2, [phi(t, θ) for t in t_] .- u_)) / length(u_)
+end
+
+alg1 = NeuralPDE.NNODE(luxchain, opt, strategy = GridTraining(0.01),
+                       additional_loss = additional_loss)
+
+sol1 = solve(prob, alg1, verbose = true, abstol = 1.0f-8, maxiters = 500)
+@test sol1.errors[:l2] < 0.5
+
+# QuadratureTraining (Flux Chain)
+chain = Flux.Chain(Dense(1, 5, σ), Dense(5, 1))
+
+(u_, t_) = (u_analytical(ts), ts)
+function additional_loss(phi, θ)
+    return sum(sum(abs2, [phi(t, θ) for t in t_] .- u_)) / length(u_)
+end
+
+alg1 = NeuralPDE.NNODE(chain, opt, additional_loss = additional_loss)
+
+sol1 = solve(prob, alg1, verbose = true, abstol = 1.0f-10, maxiters = 200)
+@test sol1.errors[:l2] < 0.5
+
+# QuadratureTraining (Lux Chain)
+luxchain = Lux.Chain(Lux.Dense(1, 5, Lux.σ), Lux.Dense(5, 1))
+
+(u_, t_) = (u_analytical(ts), ts)
+function additional_loss(phi, θ)
+    return sum(sum(abs2, [phi(t, θ) for t in t_] .- u_)) / length(u_)
+end
+
+alg1 = NeuralPDE.NNODE(luxchain, opt, additional_loss = additional_loss)
+
+sol1 = solve(prob, alg1, verbose = true, abstol = 1.0f-10, maxiters = 200)
+@test sol1.errors[:l2] < 0.5
+
+# StochasticTraining(Flux Chain)
+chain = Flux.Chain(Dense(1, 5, σ), Dense(5, 1))
+
+(u_, t_) = (u_analytical(ts), ts)
+function additional_loss(phi, θ)
+    return sum(sum(abs2, [phi(t, θ) for t in t_] .- u_)) / length(u_)
+end
+
+alg1 = NeuralPDE.NNODE(chain, opt, strategy = StochasticTraining(1000),
+                       additional_loss = additional_loss)
+
+sol1 = solve(prob, alg1, verbose = true, abstol = 1.0f-8, maxiters = 500)
+@test sol1.errors[:l2] < 0.5
+
+# StochasticTraining (Lux Chain)
+luxchain = Lux.Chain(Lux.Dense(1, 5, Lux.σ), Lux.Dense(5, 1))
+
+(u_, t_) = (u_analytical(ts), ts)
+function additional_loss(phi, θ)
+    return sum(sum(abs2, [phi(t, θ) for t in t_] .- u_)) / length(u_)
+end
+
+alg1 = NeuralPDE.NNODE(luxchain, opt, strategy = StochasticTraining(1000),
+                       additional_loss = additional_loss)
+
+sol1 = solve(prob, alg1, verbose = true, abstol = 1.0f-8, maxiters = 500)
+@test sol1.errors[:l2] < 0.5
