4197cdb0595e5fa853741c15892233ed05016339
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2023-04-03 20:18:48 +0530

    rebased

--- test/NNODE_tests.jl
+++ test/NNODE_tests.jl
@@ -207,6 +207,7 @@ sol = solve(prob, NeuralPDE.NNODE(luxchain, opt; batch = true), verbose = true,
             abstol = 1.0f-8, dt = 1 / 5.0f0)
 @test sol.errors[:l2] < 0.5
 
+# WeightedIntervalTraining(Lux Chain)
 function f(u, p, t)
     [p[1] * u[1] - p[2] * u[1] * u[2], -p[3] * u[2] + p[4] * u[1] * u[2]]
 end
