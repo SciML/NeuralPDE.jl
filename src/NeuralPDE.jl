c8b5dbac3395a8684e74bd8d40de808aa12f33d0
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2023-06-12 00:14:29 +0530

    model + custom likelihood + adding tests

--- src/NeuralPDE.jl
+++ src/NeuralPDE.jl
@@ -3,16 +3,6 @@ $(DocStringExtensions.README)
 """
 module NeuralPDE
 
-# Fixes #682
-using Turing, MacroTools, LinearAlgebra
-using Parameters, Distributions, Optim, Requires
-using Distances, DocStringExtensions, Random, StanSample
-using DynamicHMC, TransformVariables, LogDensityProblemsAD, TransformedLogDensities
-STANDARD_PROB_GENERATOR(prob, p) = remake(prob; u0 = eltype(p).(prob.u0), p = p)
-function STANDARD_PROB_GENERATOR(prob::EnsembleProblem, p)
-    EnsembleProblem(remake(prob.prob; u0 = eltype(p).(prob.prob.u0), p = p))
-end
-
 using DocStringExtensions
 using Reexport, Statistics
 @reexport using DiffEqBase
@@ -48,9 +38,6 @@ abstract type AbstractPINN end
 
 abstract type AbstractTrainingStrategy end
 
-# Fixes #682
-include("turing_MCMC.jl")
-
 include("pinn_types.jl")
 include("symbolic_utilities.jl")
 include("training_strategies.jl")
@@ -60,6 +47,9 @@ include("rode_solve.jl")
 include("transform_inf_integral.jl")
 include("discretize.jl")
 include("neural_adapter.jl")
+
+# fixes #682
+include("turing_MCMC.jl")
 
 export NNODE, TerminalPDEProblem, NNPDEHan, NNPDENS, NNRODE,
        KolmogorovPDEProblem, NNKolmogorov, NNStopping, ParamKolmogorovPDEProblem,
@@ -75,7 +65,7 @@ export NNODE, TerminalPDEProblem, NNPDEHan, NNPDENS, NNRODE,
        MiniMaxAdaptiveLoss,
        LogOptions
 
-#   Fixes #682
-export turing_MCMC
+#fixes #682
+export bayesian_pinn_ode
 
 end # module
--- src/turing_MCMC.jl
+++ src/turing_MCMC.jl
@@ -1,34 +1,72 @@
 using Turing
 
-function pinn(prob::DiffEqBase.DEProblem, parameters, dataset, recon)
-    chain = recon(parameters)
+struct odeByNN{C, T, U}
+    chain::C
+    u0::U
+    t0::T
 
-    # output PINN predictions
-    sol
+    function odeByNN(re::Optimisers.Restructure, t, u0)
+        new{typeof(re), typeof(t), typeof(u0)}(re, t, u0)
+    end
 end
 
-myloglikelihood(x, μ) = loglikelihood(MvNormal(μ, 1), x)
+function generate_phi(chain::Flux.Chain, t, u0, init_params::Nothing)
+    θ, re = Flux.destructure(chain)
+    odeByNN(re, t, u0), θ
+end
 
+# nn OUTPUT AT t
+function (f::odeByNN{C, T, U})(t::AbstractVector,
+                               θ) where {C <: Optimisers.Restructure, T, U}
+    f.u0 .+ (t - f.t0) * f.chain(θ)(adapt(parameterless_type(θ), t'))
+end
+
+# ODE DU/DX
+function NNodederi(phi::odeByNN, t::AbstractVector, θ, autodiff::Bool)
+    if autodiff
+        ForwardDiff.jacobian(t -> phi(t, θ), t)
+    else
+        (phi(t + sqrt(eps(typeof(t)))) - phi(t, θ)) / sqrt(eps(typeof(t)))
+    end
+end
+
+function physloglikelihood(prob::DiffEqBase.DEProblem, pred, dataset; var = 0.5)
+    u0 = prob.u0
+    tspan = prob.tspan
+    t0 = tspan[1]
+    p = prob.p
+    f = prob.f
+
+    autodiff = false
+    phi, initparams = generate_phi(chain, t0, u0, nothing)
+    μ = [f(phi(t, θ), p, u0) for t in tspan]
+    physsol = NNodederi(phi, t, θ, autodiff)
+    loglikelihood(MvNormal(μ, var), physsol)
+end
+
+# dataset would be (x̂,t)
 # priors: pdf for W,b + pdf for ODE params
-function bayesian_pinn_ode(prob::DiffEqBase.DEProblem, chain, priors, dataset, ts;
-                           sampling_strategy = NUTS(0.65), num_samples = 1000,
-                           syms = [Turing.@varname(theta[i]) for i in 1:length(priors)])
+function bayesian_pinn_ode(prob::DiffEqBase.DEProblem, chain, dataset;
+                           sampling_strategy = NUTS(0.65), num_samples = 1000)
     param_initial, recon = Flux.destructure(chain)
     nparameters = length(param_initial)
 
     alpha = 0.09
     sig = sqrt(1.0 / alpha)
     @model function bayes_pinn(dataset)
-        theta = Vector{T}{undef, length(priors)}
-        for i in eachindex(priors)
-            theta[i] ~ NamedDist(priors[i], sym[i])
-        end
-        nnparameters ~ MvNormal(zeros(nparameters), sig .* ones(nparameters))
+        # parameter estimation?
 
-        preds = pinn(prob, parameters, dataset, recon)
-        for i in eachindex(ts)
-            datapoints ~ MvNormal(pred)
-            Turing.@addlogprob! physloglikelihood(pred, μ)
+        # prior for NN parameters(not included bias yet?)
+        nnparameters ~ MvNormal(zeros(nparameters), sig .* ones(nparameters))
+        nn = recon(nnparameters)
+        preds = nn(dataset[:2])
+
+        # likelihood for dataset vs NN pred
+        dataset[:1] ~ MvNormal(preds, sig .* ones(length(dataset[:2])))
+
+        # likelihood for NN pred vs Equation satif
+        if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
+            Turing.@addlogprob! physloglikelihood(prob, pred, dataset, var = sig)
         end
     end
 
@@ -37,6 +75,7 @@ function bayesian_pinn_ode(prob::DiffEqBase.DEProblem, chain, priors, dataset, t
     return ch
 end
 
+# Testing out Code (will put in bpinntests.jl file in tests directory)
 function pendulum(du, u, p, t)
     ω, L = p
     x, y = u
@@ -46,4 +85,9 @@ end
 
 u0 = [1.0, 0.1]
 tspan = (0.0, 10.0)
-prob1 = ODEProblem(pendulum, u0, tspan, [1.0, 2.5])
+
+p = [1.0, 2.5]
+prob1 = ODEProblem(pendulum, u0, tspan, p)
+nnt = Chain(Dense(1, 3, tanh), Dense(3, 4, tanh), Dense(4, 1, sigmoid))
+dataset = []
+bayesian_pinn_ode(prob1, nnt, dataset)
