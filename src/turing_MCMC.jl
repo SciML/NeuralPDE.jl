56e7afcd3fb64552e553ec7574137988026684dc
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2023-06-17 20:40:57 +0530

    It works but predictions are superbad

--- src/turing_MCMC.jl
+++ src/turing_MCMC.jl
@@ -1,5 +1,4 @@
 using Turing
-
 struct odeByNN{C, T, U}
     chain::C
     u0::U
@@ -10,84 +9,98 @@ struct odeByNN{C, T, U}
     end
 end
 
-function generate_phi(chain::Flux.Chain, t, u0, init_params::Nothing)
+@time function generate_phi(chain::Flux.Chain, t, u0, init_params::Nothing)
     θ, re = Flux.destructure(chain)
     odeByNN(re, t, u0), θ
 end
 
 # nn OUTPUT AT t
+@time function (f::odeByNN{C, T, U})(t::Number,
+                                     θ) where {C <: Optimisers.Restructure, T, U}
+    f.u0 + (t - f.t0) * first(f.chain(θ)(adapt(parameterless_type(θ), [t])))
+end
+
 function (f::odeByNN{C, T, U})(t::AbstractVector,
                                θ) where {C <: Optimisers.Restructure, T, U}
-    f.u0 .+ (t - f.t0) * f.chain(θ)(adapt(parameterless_type(θ), t'))
+    f.u0 .+ (t .- f.t0) .* f.chain(θ)(adapt(parameterless_type(θ), t'))
 end
 
 # ODE DU/DX
+function NNodederi(phi::odeByNN, t::Number, θ, autodiff::Bool)
+    if autodiff
+        ForwardDiff.jacobian(t -> phi(t, θ), t)
+    else
+        (phi(t + sqrt(eps(typeof(t))), θ) - phi(t, θ)) / sqrt(eps(typeof(t)))
+    end
+end
+
 function NNodederi(phi::odeByNN, t::AbstractVector, θ, autodiff::Bool)
     if autodiff
         ForwardDiff.jacobian(t -> phi(t, θ), t)
     else
-        (phi(t + sqrt(eps(typeof(t)))) - phi(t, θ)) / sqrt(eps(typeof(t)))
+        (phi(t .+ sqrt(eps(eltype(t))), θ) - phi(t, θ)) ./ sqrt(eps(eltype(t)))
     end
 end
 
-function physloglikelihood(prob::DiffEqBase.DEProblem, pred, dataset; var = 0.5)
+@time function physloglikelihood(chain::Any, prob::DiffEqBase.DEProblem,
+                                 t::AbstractVector; var = 0.5)
     u0 = prob.u0
-    tspan = prob.tspan
-    t0 = tspan[1]
+    t0 = t[1]
     p = prob.p
     f = prob.f
-
+    # let this be(will fix)
     autodiff = false
+
     phi, initparams = generate_phi(chain, t0, u0, nothing)
-    μ = [f(phi(t, θ), p, u0) for t in tspan]
-    physsol = NNodederi(phi, t, θ, autodiff)
-    loglikelihood(MvNormal(μ, var), physsol)
+
+    μ = vec([f(phi(t[i], initparams), p, u0) for i in eachindex(t)])
+    physsol = vec([NNodederi(phi, t[i], initparams, autodiff) for i in eachindex(t)])
+
+    # To reduce heap allocations but some erros came up
+    # μ = similar(t)
+    # physsol = similar(t)
+    # μ = f(phi(t, initparams), p, u0)
+    # physsol = NNodederi(phi, t, initparams, autodiff)
+    # for i in eachindex(t)
+    #     μ[i] = f(phi(t[i], initparams), p, u0)
+    #     physsol[i] = NNodederi(phi, t[i], initparams, autodiff)
+    # end
+
+    return loglikelihood(MvNormal(μ, var .* ones(Float64, length(t))), physsol)
 end
 
 # dataset would be (x̂,t)
 # priors: pdf for W,b + pdf for ODE params
-function bayesian_pinn_ode(prob::DiffEqBase.DEProblem, chain, dataset;
-                           sampling_strategy = NUTS(0.65), num_samples = 1000)
+@time function bayesian_pinn_ode(prob::DiffEqBase.DEProblem, chain, dataset;
+                                 sampling_strategy = NUTS(0.9), num_samples = 1000)
     param_initial, recon = Flux.destructure(chain)
     nparameters = length(param_initial)
 
+    if isinplace(prob)
+        throw(error("The BPINN ODE solver only supports out-of-place ODE definitions, i.e. du=f(u,p,t)."))
+    end
+
     alpha = 0.09
     sig = sqrt(1.0 / alpha)
-    @model function bayes_pinn(dataset)
+    # physloglikelihood(chain, prob, dataset[2], var = sig)
+    DynamicPPL.@model function bayes_pinn(dataset)
         # parameter estimation?
 
         # prior for NN parameters(not included bias yet?) - P(Θ)
         nnparameters ~ MvNormal(zeros(nparameters), sig .* ones(nparameters))
         nn = recon(nnparameters)
-        preds = nn(dataset[:2])
+        preds = nn(dataset[2]')
 
-        # likelihood for dataset vs NN pred  - P( X̄ | Θ)
-        dataset[:1] ~ MvNormal(preds, sig .* ones(length(dataset[:2])))
-
-        # likelihood for NN pred vs Equation satif - P(phys | Θ)
+        # # likelihood for NN pred vs Equation satif - P(phys | Θ)
         if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
-            Turing.@addlogprob! physloglikelihood(prob, pred, dataset, var = sig)
+            Turing.@addlogprob! physloglikelihood(nn, prob, dataset[2], var = sig)
         end
+
+        # # likelihood for dataset vs NN pred  - P( X̄ | Θ)
+        dataset[1] ~ MvNormal(vec(preds), sig .* ones(length(dataset[2])))
     end
 
     model = bayes_pinn(dataset)
     ch = sample(model, sampling_strategy, num_samples)
     return ch
 end
-
-# Testing out Code (will put in bpinntests.jl file in tests directory)
-function pendulum(du, u, p, t)
-    ω, L = p
-    x, y = u
-    du[1] = y
-    du[2] = -ω * y - (9.8 / L) * sin(x)
-end
-
-u0 = [1.0, 0.1]
-tspan = (0.0, 10.0)
-
-p = [1.0, 2.5]
-prob1 = ODEProblem(pendulum, u0, tspan, p)
-nnt = Chain(Dense(1, 3, tanh), Dense(3, 4, tanh), Dense(4, 1, sigmoid))
-dataset = []
-bayesian_pinn_ode(prob1, nnt, dataset)
