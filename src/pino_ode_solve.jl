5fc72b9708acb66e65e3ec7e23bfb2f9784631de
KirillZubov <kirillzubov3@gmail.com>
2024-02-13 17:27:14 +0400

    pino ode prot

--- src/pino_ode_solve.jl
+++ src/pino_ode_solve.jl
@@ -1,9 +1,9 @@
-struct PINOODE{C, O, T, P, B, K} <: DiffEqBase.AbstractODEAlgorithm
+struct PINOODE{C, O, T, P, K} <: DiffEqBase.AbstractODEAlgorithm
     chain::C
     opt::O
     training_mapping::T
     init_params::P
-    batch::B
+    minibatch::Int
     kwargs::K
 end
 
@@ -11,77 +11,98 @@ function PINOODE(chain,
         opt,
         training_mapping,
         init_params = nothing;
-        batch = nothing,
+        minibatch = 0,
         kwargs...)
     !(chain isa Lux.AbstractExplicitLayer) && (chain = Lux.transform(chain))
-    PINOODE(chain, opt, training_mapping, init_params, batch, kwargs)
+    PINOODE(chain, opt, training_mapping, init_params, minibatch, kwargs)
 end
 
+# mutable struct Phi{C, S}
+#     f::C
+#     st::S
+#     function Phi(chain::Lux.AbstractExplicitLayer)
+#         st = Lux.initialstates(Random.default_rng(), chain)
+#         new{typeof(chain), typeof(st)}(chain, st)
+#     end
+# end
+
+# function (f::Phi{<:Lux.AbstractExplicitLayer})(x::Number, θ)
+#     y, st = f.f(adapt(parameterless_type(ComponentArrays.getdata(θ)), [x]), θ, f.st)
+
+#     ChainRulesCore.@ignore_derivatives f.st = st
+#     y
+# end
+
+# function (f::Phi{<:Lux.AbstractExplicitLayer})(x::AbstractArray, θ)
+#     y, st = f.f(adapt(parameterless_type(ComponentArrays.getdata(θ)), x), θ, f.st)
+#     ChainRulesCore.@ignore_derivatives f.st = st
+#     y
+# end
+
+# function (f::Phi{<:Optimisers.Restructure})(x, θ)
+#     f.f(θ)(adapt(parameterless_type(θ), x))
+# end
+
+# function (f::PINOPhi{C})(t, θ) where {C <: Optimisers.Restructure}
+#     f.f(θ)(t)
+# end
+# Zygote.gradient(θ -> sum(abs2, data_loss(phi, tspan, θ, training_mapping)), init_params)
+
+# function inner_loss(phi::PINOPhi{C}, tspan, θ, training_mapping::Tuple) where {C}
+#     loss = data_loss(phi, tspan, θ, training_mapping ) #+ physics_loss()
+#     loss
+# end
+
 """
-    PINOPhi(chain::Lux.AbstractExplicitLayer, t, u0, st)
+    PINOPhi(chain::Lux.AbstractExplicitLayer, t, st)
 """
-mutable struct PINOPhi{C, T, U, S}
+mutable struct PINOPhi{C, S}
     chain::C
-    t0::T
-    u0::U
     st::S
-    function PINOPhi(chain::Lux.AbstractExplicitLayer, t::Number, u0, st)
-        new{typeof(chain), typeof(t), typeof(u0), typeof(st)}(chain, t, u0, st)
+    function PINOPhi(chain::Lux.AbstractExplicitLayer, st)
+        new{typeof(chain), typeof(st)}(chain,  st)
     end
 end
 
-function generate_pino_phi_θ(chain::Lux.AbstractExplicitLayer, t, u0, init_params)
+function generate_pino_phi_θ(chain::Lux.AbstractExplicitLayer, init_params)
     θ, st = Lux.setup(Random.default_rng(), chain)
     if init_params === nothing
         init_params = ComponentArrays.ComponentArray(θ)
     else
         init_params = ComponentArrays.ComponentArray(init_params)
     end
-    PINOPhi(chain, t, u0, st), init_params
+    PINOPhi(chain, st), init_params
 end
 
-function (f::PINOPhi{C, T, U})(t::AbstractMatrix,
-        θ) where {C <: Lux.AbstractExplicitLayer, T, U}
+function (f::PINOPhi{C})(t::Number, θ) where {C <: Lux.AbstractExplicitLayer}
+    y, st = f.chain(adapt(parameterless_type(ComponentArrays.getdata(θ)), [t]), θ, f.st)
+    ChainRulesCore.@ignore_derivatives f.st = st
+    # f.u0 .+ (t .- f.t0) .* y
+    y
+end
+
+function (f::PINOPhi{C})(t::AbstractArray, θ) where {C <: Lux.AbstractExplicitLayer}
     # Batch via data as row vectors
-    # y, st = f.chain(adapt(parameterless_type(ComponentArrays.getdata(θ.depvar)), t'),
-        # θ.depvar,
-        # f.st)
-    y, st = f.chain(adapt(parameterless_type(ComponentArrays.getdata(θ)), t),
-        θ,
-        f.st)
+    y, st = f.chain(adapt(parameterless_type(ComponentArrays.getdata(θ)), t), θ, f.st)
     ChainRulesCore.@ignore_derivatives f.st = st
     # f.u0 .+ (t' .- f.t0) .* y
     y
 end
 
-function inner_data_loss(phi, ts, θ, a, ground_u, i)
-    u_size = size(ground_u[i])
-    a_arr = fill(a[i], u_size)
-    input_data = reduce(vcat, [ts, a_arr])
-    y = phi(input_data, θ)
-    y - ground_u[i]
-end
-function data_loss(phi,tspan, θ, training_mapping)
-    a, ground_u = training_mapping
-    t0, t_end = tspan
-    size_1 = size(ground_u[1])
-    size_2 = size(ground_u)[1]
-    ts = Float32.(reshape(collect(range(t0, stop = t_end, length = size_1[2])), size_1))
-    loss = [inner_data_loss(phi, ts, θ, a, ground_u,i)
-            for i in 1:size_2]
-    # reduce(vcat, loss)
-    sum(abs2, reduce(vcat, loss)) / (size_1[2] * size_2)
+function inner_data_loss(phi::PINOPhi{C}, θ, in_, out_) where {C}
+    phi(in_, θ) - out_
 end
 
-function inner_loss(phi, tspan, θ, training_mapping)
-    loss = data_loss(phi, tspan, θ, training_mapping ) #+ physics_loss()
-    loss
+function data_loss(phi::PINOPhi{C}, tspan, θ, training_mapping) where {C}
+    input_set, output_set = training_mapping
+    data_set_size = size(input_set)[1] * size(input_set[1])[2]
+    loss = reduce(vcat,[inner_data_loss(phi, θ, in_, out_) for (in_, out_) in zip(input_set, output_set)])
+    loss / data_set_size
 end
 
-function generate_loss(phi, tspan, training_mapping) #training_mapping::Tuple
+function generate_loss(phi::PINOPhi{C}, tspan, training_mapping::Tuple) where {C}
     function loss(θ, _)
-        # inner_loss(phi, f, tspan, θ, p,training_mapping )
-        sum(abs2, inner_loss(phi, tspan, θ, training_mapping))
+        sum(abs2, data_loss(phi, tspan, θ, training_mapping))
     end
     return loss
 end
@@ -89,46 +110,38 @@ end
 function DiffEqBase.__solve(prob::DiffEqBase.AbstractODEProblem,
         alg::PINOODE,
         args...;
-        dt = nothing,
+        # dt = nothing,
         abstol = 1.0f-6,
         reltol = 1.0f-3,
         verbose = false,
         saveat = nothing,
         maxiters = nothing)
-    u0 = prob.u0
+    # u0 = prob.u0 ? TODO
     tspan = prob.tspan
     f = prob.f
     p = prob.p
-    t0 = tspan[1]
     # param_estim = alg.param_estim
 
-    #hidden layer
     chain = alg.chain
     opt = alg.opt
-
-    #train points generation
     init_params = alg.init_params
 
     # mapping between functional space of some vararible 'a' of equation (for example initial
-    # codition {u(t0 x)} or parameter) and solution of equation u(t)
+    # condition {u(t0 x)} or parameter) join  and solution of equation u(t)
     training_mapping = alg.training_mapping
 
     !(chain isa Lux.AbstractExplicitLayer) &&
         error("Only Lux.AbstractExplicitLayer neural networks are supported")
 
-    phi, init_params = generate_pino_phi_θ(chain, t0, u0, init_params)
+    phi, init_params = generate_pino_phi_θ(chain, init_params)
 
-    # init_params = ComponentArrays.ComponentArray(init_params)
-    # init_params = ComponentArrays.ComponentArray(;depvar = ComponentArrays.ComponentArray(init_params))
-    @show phi(rand(2,1), init_params)
-    @show phi(rand(2, 10), init_params)
+    init_params = ComponentArrays.ComponentArray(init_params)
 
     isinplace(prob) &&
         throw(error("The PINOODE solver only supports out-of-place ODE definitions, i.e. du=f(u,p,t)."))
 
     try
-        # TODO
-        phi(rand(2, 1), init_params)
+        phi(first(training_mapping[1]), init_params)
     catch err
         if isa(err, DimensionMismatch)
             throw(DimensionMismatch("Dimensions of the initial u0 and chain should match"))
@@ -148,33 +161,22 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractODEProblem,
     # Creates OptimizationFunction Object from total_loss
     total_loss(θ, _) = inner_f(θ, phi)
 
-
     # Optimization Algo for Training Strategies
     opt_algo = Optimization.AutoZygote()
 
     # Creates OptimizationFunction Object from total_loss
     optf = OptimizationFunction(total_loss, opt_algo)
 
-    # iteration = 0
-    # callback = function (p, l)
-    #     iteration += 1
-    #     verbose && println("Current loss is: $l, Iteration: $iteration")
-    #     l < abstol
-    # end
-
+    iteration = 0
     callback = function (p, l)
-        println("Current loss is: $l")
-        return false
+        iteration += 1
+        verbose && println("Current loss is: $l, Iteration: $iteration")
+        l < abstol
     end
 
-    # init_params = ComponentArrays.ComponentArray(θ)
-
-    # Zygote.gradient(θ -> total_loss(θ, 1), init_params)
-
     optprob = OptimizationProblem(optf, init_params)
-    res = solve(optprob, opt; callback = callback, maxiters = maxiters) # alg.kwargs...)
+    res = solve(optprob, opt; callback, maxiters, alg.kwargs...)
 
     # PINOsolution(fullsolution)
-    # res
-    (total_loss,optprob,opt,callback, maxiters)
+    (res, phi)
 end
--- test/PINO_ode_tests.jl
+++ test/PINO_ode_tests.jl
@@ -3,38 +3,60 @@ using OrdinaryDiffEq,OptimizationOptimisers
 using Flux, Lux
 using Statistics, Random
 using NeuralOperators
-# using NeuralPDE
+using NeuralPDE
 
 @testset "Example 1" begin
     linear_analytic = (u0, p, t) -> u0 + sin(p * t) / (p)
     linear = (u, p, t) -> cos(p * t)
     tspan = (0.0, 2.0)
     u0 = 0.0
-    a = Float32.([i  for i in 0.1:0.05:pi])
+
+    #generate data set
+    t0, t_end = tspan
+    instances_size = 100
+    range_ = range(t0, stop = t_end, length = instances_size)
+    ts = reshape(collect(range_), 1, instances_size)
+    batch_size = 50
+    as = [i for i in range(0.1, stop = pi/2, length = batch_size)]
+
+    patamaters_set = []
+    for a_i in as
+        a_arr = fill(a_i, instances_size)'
+        s = reduce(vcat, [ts, a_arr])
+        push!(patamaters_set, s)
+    end
 
     u_output_ = []
-    for i in 1:length(a)
-        prob = ODEProblem(ODEFunction(linear, analytic = linear_analytic), u0, tspan, a[i])
-        sol1 = solve(prob, Tsit5(); saveat = 0.02)
-        push!(u_output_, Float32.(sol1.u'))
+    for a_i in as
+        prob = ODEProblem(ODEFunction(linear, analytic = linear_analytic), u0, tspan, a_i)
+        sol1 = solve(prob, Tsit5(); saveat = 0.0204)
+        push!(u_output_, sol1(range_).u')
     end
+
+    """
+    Set of training data:
+    * input data: mesh of 't' paired with set of parameters 'a':
+    * output data: set of corresponding parameter 'a' solutions u(t){a}
+     """
+    training_mapping = (patamaters_set, u_output_)
+
     #TODO u0 -> [u0,a]?
     prob = ODEProblem(linear, u0, tspan)
-    chain = Lux.Chain(Lux.Dense(2, 5, Lux.σ), Lux.Dense(5, 1))
-    # opt = OptimizationOptimisers.Adam(0.1)
+    chain = Lux.Chain(Lux.Dense(2, 20, Lux.σ), Lux.Dense(20, 20, Lux.σ), Lux.Dense(20, 1))
     opt = OptimizationOptimisers.Adam(0.03)
-    training_mapping = (a, u_output_)
+
     alg = NeuralPDE.PINOODE(chain, opt, training_mapping)
 
-    sol = solve(prob,
+    res, phi = solve(prob,
         alg, verbose = true,
-        maxiters = 2000, abstol = 1.0f-7)
+        maxiters = 4000, abstol = 1.0f-10)
 
-    total_loss
-    total_loss, optprob, opt, callback, maxiters = sol
-    phi(rand(2, 10), init_params)
 
-    total_loss(init_params, 1)
+    predict = reduce(hcat, [phi(patamaters_set[i], res.u)' for i in 1:batch_size])
+    grpound = reduce(hcat, [u_output_[i]' for i in 1:batch_size])
+    @test grpound≈predict atol=3
 
-     using Plots
+    # i = 30
+    # plot(predict[:, i])
+    # plot!(grpound[:, i])
 end
