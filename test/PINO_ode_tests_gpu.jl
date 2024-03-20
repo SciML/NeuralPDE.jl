cefb901280b963e4d7a0f2c10aea9c3a6bd1257a
KirillZubov <kirillzubov3@gmail.com>
2024-03-20 16:58:00 +0400

    fix

--- test/PINO_ode_tests_gpu.jl
+++ test/PINO_ode_tests_gpu.jl
@@ -109,7 +109,7 @@ end
 
     opt = OptimizationOptimisers.Adam(0.001)
     alg = PINOODE(
-        flat_no, opt, train_set; init_params = ps, is_data_loss = true, is_physics_loss = true)
+        chain, opt, train_set; init_params = ps, is_data_loss = true, is_physics_loss = true)
     pino_solution = solve(prob, alg, verbose = false, maxiters = 4000)
     predict = pino_solution.predict |> cpu
     ground = u_output_
