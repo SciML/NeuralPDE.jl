a28d12f065a899ebff0eb9f36b833d1559ad947e
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2024-03-26 20:55:24 +0530

    update NeuralPDE.jl

--- src/NeuralPDE.jl
+++ src/NeuralPDE.jl
@@ -53,6 +53,7 @@ include("advancedHMC_MCMC.jl")
 include("BPINN_ode.jl")
 include("PDE_BPINN.jl")
 include("dgm.jl")
+include("collocated_estim.jl")
 
 export NNODE, NNDAE,
     PhysicsInformedNN, discretize,
