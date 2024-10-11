2ea5212e0c0c1f2bb49c10010b7f37ff6e77a68d
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2024-10-11 16:14:10 +0530

    tests pass locally

--- test/BPINN_PDEinvsol_tests.jl
+++ test/BPINN_PDEinvsol_tests.jl
@@ -34,33 +34,7 @@ Random.seed!(100)
     u1 = u1 .+ (u1 .* 0.2) .* randn(size(u1))
     dataset = [hcat(u1, timepoints)]
 
-    # TODO: correct implementations
-    # # checking all training strategies
-    # discretization = BayesianPINN([chainl], StochasticTraining(200), param_estim = true,
-    #     dataset = [dataset, nothing])
-
-    # ahmc_bayesian_pinn_pde(pde_system,
-    #     discretization;
-    #     draw_samples = 1500,
-    #     bcstd = [0.05],
-    #     phystd = [0.01], l2std = [0.01],
-    #     priorsNNw = (0.0, 1.0),
-    #     saveats = [1 / 50.0],
-    #     param = [LogNormal(6.0, 0.5)])
-
-    # discretization = BayesianPINN([chainl], QuasiRandomTraining(200), param_estim = true,
-    #     dataset = [dataset, nothing])
-
-    # ahmc_bayesian_pinn_pde(pde_system,
-    #     discretization;
-    #     draw_samples = 1500,
-    #     bcstd = [0.05],
-    #     phystd = [0.01], l2std = [0.01],
-    #     priorsNNw = (0.0, 1.0),
-    #     saveats = [1 / 50.0],
-    #     param = [LogNormal(6.0, 0.5)])
-
-        # alternative to QuadratureTraining [WIP]
+    # TODO: correct BPINN implementations for Training Strategies.
 
     discretization = BayesianPINN([chainl], GridTraining([0.02]), param_estim = true,
         dataset = [dataset, nothing])
@@ -164,73 +138,153 @@ function recur_expression(exp, Dict_differentials)
     end
 end
 
-println("Example 3: 2D Periodic System with New parameter estimation")
-@parameters t, p
-@variables u(..)
+@testset "improvement in Solving Parametric Kuromo-Sivashinsky Equation" begin
+    @parameters x, t, α
+    @variables u(..)
+    Dt = Differential(t)
+    Dx = Differential(x)
+    Dx2 = Differential(x)^2
+    Dx3 = Differential(x)^3
+    Dx4 = Differential(x)^4
 
-Dt = Differential(t)
-eqs = Dt(u(t)) - cos(p * t) * u(t) ~ 0
-bcs = [u(0) ~ 0.0]
-domains = [t ∈ Interval(0.0, 2.0)]
+    # α = 1 (KS equation to be parametric in a)
+    β = 4
+    γ = 1
+    eq = Dt(u(x, t)) + u(x, t) * Dx(u(x, t)) + α * Dx2(u(x, t)) + β * Dx3(u(x, t)) + γ * Dx4(u(x, t)) ~ 0
 
-chainl = Lux.Chain(Lux.Dense(1, 6, tanh), Lux.Dense(6, 1))
-initl, st = Lux.setup(Random.default_rng(), chainl)
+    u_analytic(x, t; z = -x / 2 + t) = 11 + 15 * tanh(z) - 15 * tanh(z)^2 - 15 * tanh(z)^3
+    du(x, t; z = -x / 2 + t) = 15 / 2 * (tanh(z) + 1) * (3 * tanh(z) - 1) * sech(z)^2
 
-@named pde_system = PDESystem(eqs,
-    bcs,
-    domains,
-    [t],
-    [u(t)],
-    [p],
-    defaults = Dict([p => 4.0]))
+    bcs = [u(x, 0) ~ u_analytic(x, 0),
+        u(-10, t) ~ u_analytic(-10, t),
+        u(10, t) ~ u_analytic(10, t),
+        Dx(u(-10, t)) ~ du(-10, t),
+        Dx(u(10, t)) ~ du(10, t)]
 
-analytic_sol_func1(u0, t) = u0 + sin(2 * π * t) / (2 * π)
-timepoints = collect(0.0:(1 / 100.0):2.0)
-u1 = [analytic_sol_func1(0.0, timepoint) for timepoint in timepoints]
-u1 = u1 .+ (u1 .* 0.2) .* randn(size(u1))
-dataset = [hcat(u1, timepoints)]
+    # Space and time domains
+    domains = [x ∈ Interval(-10.0, 10.0),
+        t ∈ Interval(0.0, 1.0)]
 
-discretization = BayesianPINN([chainl], GridTraining([0.02]), param_estim = true,
-    dataset = [dataset, nothing])
+    # Discretization
+    dx = 0.4
+    dt = 0.2
 
-# creating dictionary for masking equations
-eqs = pde_system.eqs
-Dict_differentials = Dict()
-exps = toexpr.(eqs)
-nullobj = [recur_expression(exp, Dict_differentials) for exp in exps]
+    # Function to compute analytical solution at a specific point (x, t)
+    function u_analytic_point(x, t)
+        z = -x / 2 + t
+        return 11 + 15 * tanh(z) - 15 * tanh(z)^2 - 15 * tanh(z)^3
+    end
 
-sol1 = ahmc_bayesian_pinn_pde(pde_system,
-    discretization;
-    draw_samples = 1500,
-    bcstd = [0.05],
-    phystd = [0.01], l2std = [0.01], phystdnew = [0.05],
-    priorsNNw = (0.0, 1.0),
-    saveats = [1 / 50.0],
-    param = [LogNormal(6.0, 0.5)],
-    Dict_differentials = Dict_differentials)
+    # Function to generate the dataset matrix
+    function generate_dataset_matrix(domains, dx, dt, xlim, tlim)
+        x_values = xlim[1]:dx:xlim[2]
+        t_values = tlim[1]:dt:tlim[2]
 
-sol2 = ahmc_bayesian_pinn_pde(pde_system,
-    discretization;
-    draw_samples = 1500,
-    bcstd = [0.05],
-    phystd = [0.01], l2std = [0.01],
-    priorsNNw = (0.0, 1.0),
-    saveats = [1 / 50.0],
-    param = [LogNormal(6.0, 0.5)])
+        dataset = []
 
-param = 2 * π
-ts = vec(sol1.timepoints[1])
-u_real = [analytic_sol_func1(0.0, t) for t in ts]
-u_predict = pmean(sol1.ensemblesol[1])
+        for t in t_values
+            for x in x_values
+                u_value = u_analytic_point(x, t)
+                push!(dataset, [u_value, x, t])
+            end
+        end
 
-@test u_predict≈u_real atol=1.5
-@test mean(u_predict .- u_real) < 0.1
-@test sol1.estimated_de_params[1]≈param atol=param * 0.3
+        return vcat([data' for data in dataset]...)
+    end
 
-ts = vec(sol2.timepoints[1])
-u_real = [analytic_sol_func1(0.0, t) for t in ts]
-u_predict = pmean(sol2.ensemblesol[1])
+    # considering sparse dataset from half of x's domain
+    datasetpde_new = [generate_dataset_matrix(domains, dx, dt, [-10, 0], [0.0, 1.0])]
 
-@test u_predict≈u_real atol=1.5
-@test mean(u_predict .- u_real) < 0.1
-@test sol1.estimated_de_params[1]≈param atol=param * 0.3
+    # Adding Gaussian noise with a 0.8 std
+    noisydataset_new = deepcopy(datasetpde_new)
+    noisydataset_new[1][:, 1] = noisydataset_new[1][:, 1] .+
+                                (randn(size(noisydataset_new[1][:, 1])) .* 0.8)
+
+    # Neural network
+    chain = Lux.Chain(Lux.Dense(2, 8, Lux.tanh),
+        Lux.Dense(8, 8, Lux.tanh),
+        Lux.Dense(8, 1))
+
+    # Discretization for old and new models
+    discretization = NeuralPDE.BayesianPINN([chain],
+        GridTraining([dx, dt]), param_estim = true, dataset = [noisydataset_new, nothing])
+
+    # let α default to 2.0
+    @named pde_system = PDESystem(eq,
+        bcs,
+        domains,
+        [x, t],
+        [u(x, t)],
+        [α],
+        defaults = Dict([α => 2.0]))
+
+    # neccesarry for loss function contruction (involves Operator masking) 
+    eqs = pde_system.eqs
+    Dict_differentials = Dict()
+    exps = toexpr.(eqs)
+    nullobj = [recur_expression(exp, Dict_differentials) for exp in exps]
+
+    # Dict_differentials is now ;
+    # Dict{Any, Any} with 5 entries:
+    #   Differential(x)(Differential(x)(u(x, t)))            => diff_5
+    #   Differential(x)(Differential(x)(Differential(x)(u(x… => diff_1
+    #   Differential(x)(Differential(x)(Differential(x)(Dif… => diff_2
+    #   Differential(x)(u(x, t))                             => diff_4
+    #   Differential(t)(u(x, t))                             => diff_3
+
+    # using HMC algorithm due to convergence, stability, time of training. (refer to mcmc chain plots)
+    # choice of std for objectives is very important
+    # pass in Dict_differentials, phystdnew arguments when using the new model
+
+    sol_new = ahmc_bayesian_pinn_pde(pde_system,
+        discretization;
+        draw_samples = 150,
+        bcstd = [0.1, 0.1, 0.1, 0.1, 0.1], phystdnew = [0.2],
+        phystd = [0.2], l2std = [0.5], param = [Distributions.Normal(2.0, 2)],
+        priorsNNw = (0.0, 1.0),
+        saveats = [1 / 100.0, 1 / 100.0],
+        Dict_differentials = Dict_differentials,
+        progress = true)
+
+    sol_old = ahmc_bayesian_pinn_pde(pde_system,
+        discretization;
+        draw_samples = 150,
+        bcstd = [0.1, 0.1, 0.1, 0.1, 0.1],
+        phystd = [0.2], l2std = [0.5], param = [Distributions.Normal(2.0, 2)],
+        priorsNNw = (0.0, 1.0),
+        saveats = [1 / 100.0, 1 / 100.0],
+        progress = true)
+
+    phi = discretization.phi[1]
+    xs, ts = [infimum(d.domain):dx:supremum(d.domain)
+              for (d, dx) in zip(domains, [dx / 10, dt])]
+    u_real = [[u_analytic(x, t) for x in xs] for t in ts]
+
+    u_predict_new = [[first(pmean(phi([x, t], sol_new.estimated_nn_params[1]))) for x in xs]
+                     for t in ts]
+
+    diff_u_new = [[abs(u_analytic(x, t) -
+                       first(pmean(phi([x, t], sol_new.estimated_nn_params[1]))))
+                   for x in xs]
+                  for t in ts]
+
+    u_predict_old = [[first(pmean(phi([x, t], sol_old.estimated_nn_params[1]))) for x in xs]
+                     for t in ts]
+    diff_u_old = [[abs(u_analytic(x, t) -
+                       first(pmean(phi([x, t], sol_old.estimated_nn_params[1]))))
+                   for x in xs]
+                  for t in ts]
+
+    @test all(all, [((diff_u_new[i]) .^ 2 .< 0.5) for i in 1:6]) == true
+    @test all(all, [((diff_u_old[i]) .^ 2 .< 0.5) for i in 1:6]) == false
+
+    MSE_new = [sum(abs2, diff_u_new[i]) for i in 1:6]
+    MSE_old = [sum(abs2, diff_u_old[i]) for i in 1:6]
+    @test (MSE_new .< MSE_old) == [1, 1, 1, 1, 1, 1]
+
+    param_new = sol_new.estimated_de_params[1]
+    param_old = sol_old.estimated_de_params[1]
+    α = 1
+    @test abs(param_new - α) < 0.2 * α
+    @test abs(param_new - α) < abs(param_old - α)
+end
