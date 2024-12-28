eb3bf92e640f5cc913fa91d3ddce77501b2be733
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2024-12-28 17:26:49 +0530

    Added test deps

--- test/NN_SDE_tests.jl
+++ test/NN_SDE_tests.jl
@@ -30,7 +30,7 @@
 end
 
 @testitem "Test-2" tags=[:nnsde] begin
-    using OrdinaryDiffEq, Random, Lux, Optimisers, DifferentialEquations
+    using OrdinaryDiffEq, Random, Lux, Optimisers, DiffEqNoiseProcess
     using OptimizationOptimJL: BFGS
     Random.seed!(100)
 
