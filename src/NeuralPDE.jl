363b8052a0ee5bc23b96022612a8ea0b081d6494
paramthakkar123 <paramthakkar864@gmail.com>
2025-01-28 22:37:24 +0530

    Circular

--- src/NeuralPDE.jl
+++ src/NeuralPDE.jl
@@ -90,8 +90,8 @@ include("advancedHMC_MCMC.jl")
 include("dgm.jl")
 
 using BayesianNeuralPDE
-export BNNODE, ahmc_bayesian_pinn_ode, ahmc_bayesian_pinn_pde,
-       BPINNsolution, BayesianPINN
+export ahmc_bayesian_pinn_ode, ahmc_bayesian_pinn_pde,
+       BNNODE, BPINNsolution, BayesianPINN
 
 export PINOODE
 export NNODE, NNDAE
