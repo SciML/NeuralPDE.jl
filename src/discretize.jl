ea7c904b271e0f9e190e971a0383e75a04b400e9
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2025-06-24 15:14:56 -0400

    Fixes Cannot solve on GPU when `PDESystem` has parameters  #913 by having `build_symbolic_loss_function` previously grab equation parameters as scalars instead of single-element arrays (which were on CPU even when network parameters were on GPU)

--- src/discretize.jl
+++ src/discretize.jl
@@ -88,7 +88,7 @@ function build_symbolic_loss_function(pinnrep::PINNRepresentation, eqs;
         params_symbols = Symbol[]
         expr_params = Expr[]
         for (i, eq_param) in enumerate(eq_params)
-            push!(expr_params, :(ArrayInterface.allowed_getindex(p, ($i):($i))))
+            push!(expr_params, :(ArrayInterface.allowed_getindex(p, ($i))))
             push!(params_symbols, Symbol(:($eq_param)))
         end
         params_eq = Expr(:(=), build_expr(:tuple, params_symbols),
