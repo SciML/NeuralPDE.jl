ad30ab2d616a2de9d22d986cf492a1271433749a
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-10 01:16:43 -0400

    Alias legacy NNPDE test group
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
 using SciMLTesting
-run_tests()
+
+if current_group() == "NNPDE"
+    for group in ("NNPDE1", "NNPDE2")
+        withenv("GROUP" => group) do
+            run_tests(; test_dir = @__DIR__)
+        end
+    end
+else
+    run_tests()
+end
